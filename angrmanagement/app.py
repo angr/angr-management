1d8017256540d907f320512b31e915366c05f6a7
Yan <yans@yancomm.net>
2014-08-05 14:05:11 -0700

    implemented surveyor logic, with support for creating surveyors (with custom arguments), stepping them forward, and suspending and resuming paths

--- angrmanagement/app.py
+++ angrmanagement/app.py
@@ -1,11 +1,20 @@
 import functools
 import json
+import ast
 import os
 
 import flask
 from werkzeug.utils import secure_filename
 import angr
 from simuvex import SimIRSB, SimProcedure
+
+try:
+    import standard_logging #pylint:disable=W0611
+    import angr_debug #pylint:disable=W0611
+except ImportError:
+    pass
+
+from .serializer import Serializer
 
 def jsonize(func):
     @functools.wraps(func)
@@ -14,7 +23,9 @@ def jsonize(func):
     return jsonned
 
 app = flask.Flask(__name__, static_folder='../static')
+the_serializer = Serializer()
 active_projects = {}
+active_surveyors = {}
 
 ROOT = os.environ.get('ANGR_MANAGEMENT_ROOT', '.')
 PROJDIR = ROOT + '/projects/'
@@ -31,7 +42,7 @@ def list_projects():
 @app.route('/api/projects/', methods=('POST',))
 @jsonize
 def new_project():
-    file = flask.request.files['file']
+    file = flask.request.files['file'] #pylint:disable=W0622
     metadata = json.loads(flask.request.form['metadata'])
     name = secure_filename(metadata['name'])
     os.mkdir(PROJDIR + name)
@@ -49,6 +60,71 @@ def activate_project(name):
                                              default_analysis_mode='symbolic',
                                              use_sim_procedures=True,
                                              arch=str(metadata['arch']))
+
+#
+# Surveyor functionality
+#
+
+@app.route('/api/surveyor_types')
+@jsonize
+def surveyor_types():
+    return angr.surveyors.all_surveyors.keys()
+
+@app.route('/api/projects/<project_name>/surveyors/new/<surveyor_type>', methods=('POST',))
+@jsonize
+def new_surveyor(project_name, surveyor_type):
+    # TODO: take a SimExit as a starting point
+
+    kwargs = dict(flask.request.json.get('kwargs', {}))
+    for k,v in kwargs.items():
+        if type(v) in (str,unicode) and v.startswith("PYTHON:"):
+            kwargs[k] = ast.literal_eval(v[7:])
+
+    p = active_projects[project_name]
+    s = angr.surveyors.all_surveyors[surveyor_type](p, **kwargs)
+    active_surveyors[str(id(s))] = s
+    return the_serializer.serialize(s)
+
+@app.route('/api/projects/<project_name>/surveyors')
+@jsonize
+def list_surveyors(project_name):
+    p = active_projects[project_name]
+    return [ the_serializer.serialize(s) for s in active_surveyors.itervalues() if s._project is p ]
+
+@app.route('/api/projects/<project_name>/surveyors/<surveyor_id>')
+@jsonize
+def get_surveyor(project_name, surveyor_id): #pylint:disable=W0613
+    return the_serializer.serialize(active_surveyors[surveyor_id])
+
+@app.route('/api/projects/<project_name>/surveyors/<surveyor_id>/step', methods=('POST',))
+@jsonize
+def step_surveyors(project_name, surveyor_id): #pylint:disable=W0613
+    steps = ( flask.request.json if flask.request.json is not None else flask.request.form ).get('steps', 1)
+    s = active_surveyors[surveyor_id]
+    s.run(n=int(steps))
+    return the_serializer.serialize(s)
+
+@app.route('/api/projects/<project_name>/surveyors/<surveyor_id>/resume/<path_id>', methods=('POST',))
+@jsonize
+def surveyor_resume_path(project_name, surveyor_id, path_id): #pylint:disable=W0613
+    s = active_surveyors[surveyor_id]
+    for list_name in s.path_lists:
+        path_list = getattr(s, list_name)
+        for p in path_list:
+            if str(id(p)) == path_id:
+                path_list.remove(p)
+                s.active.append(p)
+                return the_serializer.serialize(active_surveyors[surveyor_id])
+
+@app.route('/api/projects/<project_name>/surveyors/<surveyor_id>/suspend/<path_id>', methods=('POST',))
+@jsonize
+def surveyor_suspend_path(project_name, surveyor_id, path_id): #pylint:disable=W0613
+    s = active_surveyors[surveyor_id]
+    for p in s.active:
+        if str(id(p)) == path_id:
+            s.active.remove(p)
+            s.suspended.append(p)
+            return the_serializer.serialize(active_surveyors[surveyor_id])
 
 def serialize_simrun(run):
     if isinstance(run, SimIRSB):
--- angrmanagement/serializer.py
+++ angrmanagement/serializer.py
@@ -13,13 +13,29 @@ class Serializer(object):
         if isinstance(o, simuvex.SimState):
             return self._serialize_state(o)
         else:
-            raise Exception("Can't serialize %s", o)
+            return "NOT SERIALIZED: %s" % o
 
     def _serialize_state(self, s):
         raise NotImplementedError("TODO")
 
     def _serialize_path(self, p):
-        raise NotImplementedError("TODO")
+        return {
+            'id': id(p),
+            'length': p.length,
+            'extra_length': p.extra_length,
+            'backtrace': p.backtrace,
+            'addr_backtrace': p.addr_backtrace,
+            'callstack': p.callstack,
+            'blockcounter_stack': p.blockcounter_stack,
+            'refs': [ self.serialize(r) for r in p.refs() ],
+        }
 
     def _serialize_surveyor(self, s):
-        return { 'id': s, 'active': 'TODO' }
+        return {
+            'id': id(s),
+            'type': s.__class__.__name__,
+            'path_lists': { n:[ self.serialize(p) for p in getattr(s, n) ] for n in s.path_lists },
+            'step': s._current_step,
+            'max_concurrency': s._max_concurrency,
+            'save_deadends': s._save_deadends,
+        }
