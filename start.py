da89a87af5772e60126f0d8f6b7dba25ddc71974
Andrew Dutcher <andrew@andrewdutcher.com>
2014-10-06 13:26:58 -0700

    Scrapes projects directory for existing projects on startup

--- start.py
+++ start.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python2.7
-import sys
+import sys, os, json
 
 from angrmanagement import app
 import rpyc
 
+projects = {}
+for name in os.listdir('projects'):
+    try:
+        json.load(open('projects/' + name + '/metadata'))
+        projects[name] = None
+    except:
+        pass
+
 if len(sys.argv) > 1:
     c = rpyc.connect("localhost", int(sys.argv[1]))
-    app.app.config['PROJECTS'] = c.root.projects
-else:
-    app.app.config['PROJECTS'] = {}
+    for name, proj in c.root.projects.iteritems():
+        if name in projects:
+            print '** Warning: Project name %s found in both projects dir and connected AngrServer. Using connected project.'
+        projects[name] = proj
+
+app.app.config['PROJECTS'] = projects
 
 app.app.run(host='0.0.0.0', port=4321, debug=True)
