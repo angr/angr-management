c8b203000f8ab83e8afdb45380c64509886249e9
John Grosen <jmgrosen@gmail.com>
2015-01-26 15:47:53 -0800

    remove dummy context actions FUCKING DOUBLE CLICK

--- static/js/tile.js
+++ static/js/tile.js
@@ -18,26 +18,12 @@ tilesMod.directive('tile', function ($compile, Context) {
 		    wk.makeTilePrimary(scope.tileId);
 		},
                 keyboardShortcut: '+p' // this a horrible shortcut, but I can't think of anything better...
-            }, {
-                name: 'would be cool if this did something',
-                action: function () {alert('EVERYTHING IS COOL WHEN YOU\'RE PART OF A TEAM');},
-                keyboardShortcut: 'S+E'
-            }, {
-                name: 'woaaaahh',
-                children: [{
-                    name: 'Option 1a',
-                    action: function () {alert('option 1a pressed');}
-                },{
-                    name: 'Option 1b',
-                    action: function () {alert('option 1b pressed');}
-                }]
             }]});
 	    var interactionController = function () {
                 var pos = $(element).parent().position();
                 return {
                     coordinates: new Context.Point(pos.left, pos.top),
                     actions: interactionActions,
-                    doubleClick: function () {alert('DO A DOUBLE CLICK\n\nPRESS Z OR R TWICE');}
                 };
 	    };
 	    scope.uictx = new Context.Interactable(scope.parentUictx, $(element), interactionController, 'WORKSPACE');
