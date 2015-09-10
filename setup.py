b1cbab53abc2e4686ca4d231b636acdb43e6f3d3
Yan <yans@yancomm.net>
2015-09-09 22:35:01 -0700

    ticked version number to 4.5.9.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='angr-management',
-    version='4.5.9.9',
+    version='4.5.9.9b',
     packages=['angrmanagement', 'angrmanagement.ui', 'angrmanagement.data'],
     package_data={
         'angrmanagement.ui': ['*.enaml']
