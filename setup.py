1675abf05bd1491acd539b509fd77fd9d271f221
Low Kian Seong <lowks@users.noreply.github.com>
2015-10-31 18:49:55 +0800

    Adding `description` and `url` to `setup.py`
    Adding `description` and `url` to `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.core import setup
 setup(
     name='angr-management',
     version='4.5.9.22',
+    description='GUI for angr',
+    url='https://github.com/angr/angr-management',
     packages=['angrmanagement', 'angrmanagement.ui', 'angrmanagement.data'],
     package_data={
         'angrmanagement.ui': ['*.enaml']
