c61dd1d9b95985c8366c70d96d682cdf42b8777e
Fish <fish@cs.ucsb.edu>
2016-10-06 00:40:15 -0700

    Split WorkspaceData to SymexecView and DisasmGraphView. Support highlighting the current instruction in symexec paths.

--- angrmanagement/data/workspace.py
+++ angrmanagement/data/workspace.py
@@ -1,4 +1,4 @@
-from atom.api import Atom, Int, List, Typed, ForwardTyped, Value, Unicode
+from atom.api import Atom, Int, List, Typed, ForwardTyped, Value, Unicode, observe
 from enaml.layout.dock_layout import AreaLayout
 
 from angr import CFG, Project, Path, PathGroup
@@ -12,10 +12,7 @@ class WorkspaceData(Atom):
     # who doesn't love back-references?
     inst = ForwardTyped(lambda: Instance)
     proj = Typed(Project)
-    selected_pg = Typed(PathGroup)
-    selected_path = Typed(Path)
-    selected_function = Typed(Function)
-    selected_insn = Value()
+
     items = List()
     layout = Typed(AreaLayout)
 
@@ -25,6 +22,19 @@ class WorkspaceData(Atom):
         if self.proj is None and self.inst is not None:
             self.proj = self.inst.proj
 
+    def next_item_name(self):
+        i = self.item_idx
+        self.item_idx += 1
+        return 'item_%d' % i
+
+
+class SymexecView(WorkspaceData):
+    selected_pg = Typed(PathGroup)
+    selected_path = Typed(Path)
+
+    def __init__(self, **kwargs):
+        super(SymexecView, self).__init__(**kwargs)
+
         if self.inst is not None and len(self.inst.path_groups.groups) > 0:
             self.selected_pg = self.inst.path_groups.groups[0]
 
@@ -32,9 +42,14 @@ class WorkspaceData(Atom):
             self.selected_path = None
             self.selected_path = self.selected_pg.active[0]
 
-    def next_item_name(self):
-        i = self.item_idx
-        self.item_idx += 1
-        return 'item_%d' % i
+class DisasmGraphView(WorkspaceData):
+    selected_function = Typed(Function)
+    selected_insn = Value()
+    highlighted_insns = Typed(set)
+
+    def __init__(self, **kwargs):
+        super(DisasmGraphView, self).__init__(**kwargs)
+
+        self.highlighted_insns = set()
 
 from .instance import Instance
