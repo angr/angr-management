e361dd7e985e14e98a8c6152a639228aca1688d1
Andrew Dutcher <andrew@andrewdutcher.com>
2016-12-03 19:34:02 -0800

    use setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
 
 setup(
     name='angr-management',
     version='5.6.12.3',
     description='GUI for angr',
     url='https://github.com/angr/angr-management',
-    packages=['angrmanagement', 'angrmanagement.ui', 'angrmanagement.data', 'angrmanagement.qt',
-              'angrmanagement.utils', 'angrmanagement.widgets'],
+    packages=packages,
     package_data={
         'angrmanagement.ui': ['*.enaml']
     },
