aa4232a8be61f6d4933963e71d715dca2754d0bc
zwimer <zwimer@users.noreply.github.com>
2019-04-11 10:07:28 -0700

    Clearer error messages

--- angrmanagement/ui/dialogs/load_docker_prompt.py
+++ angrmanagement/ui/dialogs/load_docker_prompt.py
@@ -24,11 +24,17 @@ class LoadDockerPrompt(QInputDialog):
         _l.info('Docker prompt succesfully loaded')
 
 def get_docker_images(parent=None):
+    # Try to list docker images, handle failure cases
     try:
         output = subprocess.check_output(['docker', 'images', '--format', '{{.Repository}}:{{.Tag}}'])
+    except FileNotFoundError as e:
+        _l.error('Docker not found.')
+        QMessageBox(parent).critical(None, 'Docker not found', 'Make sure docker is installed and in your path')
+        raise LoadDockerPromptError
     except subprocess.CalledProcessError:
         _l.error('Docker images failed: Make sure you are have privileges.')
         QMessageBox(parent).critical(None, 'docker images failed to run', 'Are you root? Docker needs root!')
         raise LoadDockerPromptError
+
     items = output.decode('utf-8').split('\n')
     return [ i for i in items if i and '<none>' not in i ]
