f84fd6a5a7ec8ee5d41f081cd3ec50c9f8c6ba0a
Kevin Phoenix <kevin@kphoenix.us>
2019-11-04 16:58:36 -0700

    Initial support for building angr-management on azure

---
+++ .azure-pipelines/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# Create virtualenv
+python3 -m pip install --user virtualenv
+python3 -m venv venv && source venv/bin/activate
+pip install --upgrade pip
+
+# Install dependencies
+pip install keystone-engine pyinstaller
+pip install git+https://github.com/angr/archinfo.git#egg=archinfo
+pip install git+https://github.com/angr/pyvex.git#egg=pyvex
+pip install git+https://github.com/angr/cle.git#egg=cle
+pip install git+https://github.com/angr/claripy.git#egg=claripy
+pip install git+https://github.com/angr/ailment.git#egg=ailment
+pip install git+https://github.com/angr/angr.git#egg=angr
+pip install git+https://github.com/angr/archr.git#egg=archr
+
+# Install angr-mangement
+pip install -e .
+
+# Bundle!
+python bundle.py
+
+# Binary is currently at dist/start
