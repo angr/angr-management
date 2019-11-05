2d0044fb08eb4ce9ddb918aaf4d06a2939618efd
Kevin Phoenix <kevin@kphoenix.us>
2019-11-04 17:44:00 -0700

    Only install keystone and archr on Linux

--- .azure-pipelines/build.sh
+++ .azure-pipelines/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Install system dependencies
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get update
     sudo apt-get install python3-venv
 fi
@@ -11,14 +11,17 @@ python3 -m venv venv && source venv/bin/activate
 pip install --upgrade pip
 
 # Install dependencies
-pip install keystone-engine pyinstaller
+pip install pyinstaller
 pip install git+https://github.com/angr/archinfo.git#egg=archinfo
 pip install git+https://github.com/angr/pyvex.git#egg=pyvex
 pip install git+https://github.com/angr/cle.git#egg=cle
 pip install git+https://github.com/angr/claripy.git#egg=claripy
 pip install git+https://github.com/angr/ailment.git#egg=ailment
 pip install git+https://github.com/angr/angr.git#egg=angr
-pip install git+https://github.com/angr/archr.git#egg=archr
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    pip install keystone-engine
+    pip install git+https://github.com/angr/archr.git#egg=archr
+fi
 
 # Install angr-mangement
 pip install -e .
