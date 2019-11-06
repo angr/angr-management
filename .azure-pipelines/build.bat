b7ea7ac695d5ab110cf2a50de9a72647cac7118f
Kevin Phoenix <kevin@kphoenix.us>
2019-11-06 11:58:19 -0700

    Try building from batch on windows specify msvc setup script

---
+++ .azure-pipelines/build.bat
@@ -0,0 +1,10 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+pip install pyinstaller
+pip install git+https://github.com/angr/archinfo.git#egg=archinfo
+pip install git+https://github.com/angr/pyvex.git#egg=pyvex
+pip install git+https://github.com/angr/cle.git#egg=cle
+pip install git+https://github.com/angr/claripy.git#egg=claripy
+pip install git+https://github.com/angr/ailment.git#egg=ailment
+pip install git+https://github.com/angr/angr.git#egg=angr
+pip install -e .
+python bundle.py
