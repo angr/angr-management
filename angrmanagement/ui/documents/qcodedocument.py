30c4f0c4ebf1ff64f71ec151a36b4974718160d3
Fish <fish@cs.ucsb.edu>
2019-11-19 10:52:51 -0500

    Support CStructField.

--- angrmanagement/ui/documents/qcodedocument.py
+++ angrmanagement/ui/documents/qcodedocument.py
@@ -1,11 +1,9 @@
 
-from typing import Optional
-
 from PySide2.QtGui import QTextDocument
 from PySide2.QtWidgets import QPlainTextDocumentLayout
 
 from angr.analyses.decompiler.structured_codegen import CConstant, CVariable, CFunctionCall, StructuredCodeGenerator, \
-    PositionMapping
+    CStructField
 
 from ...config import Conf
 
@@ -70,6 +68,13 @@ class QCodeDocument(QTextDocument):
         elif isinstance(node, CFunctionCall):
             starts = self._codegen.nodemap.get(node.callee_func if node.callee_func is not None else node.callee_target,
                                                None)
+            if starts is None:
+                return [ ]
+
+        elif isinstance(node, CStructField):
+            key = (node.struct_type, node.offset)
+            starts = self._codegen.nodemap.get(key, None)
+
             if starts is None:
                 return [ ]
 
