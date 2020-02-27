5b5e65a442607d6beb578ce9a7b22da3f5e59387
Kevin Phoenix <kevin@kphoenix.us>
2020-02-26 20:23:44 -0700

    Add bit to delete old nightly releases

---
+++ .azure-pipelines/get_oldest_nightly.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+To avoid clutter in the releases tab on GitHub, this script has been written to
+pick an old release to delete, which is passed to a GitHubRelease task in
+./master.yml. It doesn't handle cases there are multiple old releases, but since
+this script is run after every nightly release, that shouldn't be a real issue.
+"""
+
+import github
+import re
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Missing repo argument!")
+        sys.exit(1)
+    repo_name = sys.argv[1]
+
+    g = github.Github()
+    r = g.get_repo(sys.argv[1])
+    releases = r.get_releases()
+    filtered = [x for x in releases if re.match("^nightly-\\d+$", x.tag_name)]
+
+    if len(filtered):
+        time_map = {k.created_at: k for k in filtered}
+        oldest = time_map[min(time_map.keys())]
+        result = oldest.tag_name
+    else:
+        result = ""
+
+    print(f"##vso[task.setvariable variable=old_release]{result}")
+
+
+if __name__ == "__main__":
+    main()
