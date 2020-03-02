35220a7c02f34b096043bbc8edcb8d5e7b43fb44
Fish <fish@cs.ucsb.edu>
2020-03-01 19:22:12 -0700

    CodeTaggingJob: Display a progress bar.

--- angrmanagement/data/jobs/code_tagging.py
+++ angrmanagement/data/jobs/code_tagging.py
@@ -8,12 +8,19 @@ class CodeTaggingJob(Job):
         super(CodeTaggingJob, self).__init__(name="Code tagging", on_finish=on_finish)
 
     def run(self, inst):
-        for func in inst.cfg.functions.values():
+
+        func_count = len(inst.cfg.functions)
+        for i, func in enumerate(inst.cfg.functions.values()):
             ct = inst.project.analyses.CodeTagging(func)
             func.tags = tuple(ct.tags)
+
+            percentage = i / func_count * 100
+            text = "%.02f%%" % percentage
+
+            super()._progress_callback(percentage, text=text)
 
     def finish(self, inst, result):
         super(CodeTaggingJob, self).finish(inst, result)
 
     def __repr__(self):
-        return "Tagging Code"
+        return "CodeTaggingJob"
