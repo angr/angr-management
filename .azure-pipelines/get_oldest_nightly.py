722b621ae024825b82688059acefcba3b8b745a0
Kevin Phoenix <kevin@kphoenix.us>
2020-03-02 18:15:44 -0700

    Rename nightly pipeline to nightly.yml

--- .azure-pipelines/get_oldest_nightly.py
+++ .azure-pipelines/get_oldest_nightly.py
@@ -2,8 +2,9 @@
 """
 To avoid clutter in the releases tab on GitHub, this script has been written to
 pick an old release to delete, which is passed to a GitHubRelease task in
-./master.yml. It doesn't handle cases there are multiple old releases, but since
-this script is run after every nightly release, that shouldn't be a real issue.
+./nightly.yml. It doesn't handle cases there are multiple old releases, but
+since this script is run after every nightly release, that shouldn't be a real
+issue.
 """
 
 import github
