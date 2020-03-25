75640d19103c9b560083616b4e2df041e0e4f2ac
Fish <fish@cs.ucsb.edu>
2020-03-25 01:50:26 -0700

    Fix the hotkey sequence for using docker targets.
    Ref: https://stackoverflow.com/questions/53687265/qkeysequence-differences-between-pyside-and-pyside2

--- angrmanagement/ui/menus/file_menu.py
+++ angrmanagement/ui/menus/file_menu.py
@@ -11,7 +11,7 @@ class FileMenu(Menu):
 
         self.entries.extend([
             MenuEntry('L&oad a new binary...', main_window.open_file_button, shortcut=QKeySequence(Qt.CTRL + Qt.Key_O)),
-            MenuEntry('Loa&d a new docker target...', main_window.open_docker_button, shortcut=QKeySequence(Qt.CTRL + Qt.SHIFT + Qt.Key_O)),
+            MenuEntry('Loa&d a new docker target...', main_window.open_docker_button, shortcut=QKeySequence(Qt.SHIFT + (Qt.CTRL + Qt.Key_O))),
             MenuEntry('&Save angr database...', main_window.save_database, shortcut=QKeySequence(Qt.CTRL + Qt.Key_S)),
             MenuEntry('S&ave angr database as...', main_window.save_database_as, shortcut=QKeySequence("Ctrl+Shift+S")),
             MenuSeparator(),
