f18fa7f19bf93ea19cef02c233fc3ceeff3bc138
Fish <fish@cs.ucsb.edu>
2020-04-09 00:53:59 -0700

    Improve UI responsiveness before PYSIDE-803 lands in a non-buggy PySide2.

--- angrmanagement/__init__.py
+++ angrmanagement/__init__.py
@@ -1,1 +1,9 @@
 __version__ = (8, 20, 1, 7)
+
+# Hack used to work around the slow-responsiveness issue with the GUI
+# PySide2 5.14.2 solves this problem but it introduces other bugs
+# See https://bugreports.qt.io/browse/PYSIDE-803
+# TODO: Remove it after PySide2 5.14.3 or 5.15 are released
+import sys
+sys.setswitchinterval(0.00001)
+
