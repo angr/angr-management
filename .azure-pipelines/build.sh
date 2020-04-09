e4a9e20d6c891e1d4da5b42e0d0a045e6a415de0
Kevin Phoenix <kevin@kphoenix.us>
2020-04-09 14:58:10 -0700

    Add onedir builds to nightlies

--- .azure-pipelines/build.sh
+++ .azure-pipelines/build.sh
@@ -10,7 +10,7 @@ pip install git+https://github.com/angr/claripy.git#egg=claripy
 pip install git+https://github.com/angr/ailment.git#egg=ailment
 pip install git+https://github.com/angr/angr.git#egg=angr
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    pip install keystone-engine
+    pip install keystone-engine --no-binary keystone-engine
     pip install git+https://github.com/angr/archr.git#egg=archr
 fi
 
@@ -18,14 +18,22 @@ fi
 pip install -e .
 
 # Bundle!
-python bundle.py
+python .azure-pipelines/bundle.py --onefile
+python .azure-pipelines/bundle.py --onedir
 
-# macOS apps are folders...
+mkdir upload
+
+# Prepare onefiles
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    hdiutil create /tmp/angr-management-macOS.dmg -volname "angr-management nightly" -srcfolder dist
-    rm -rf dist/*
-    mv /tmp/angr-management-macOS.dmg dist/
+    cp onefile/angr-management upload/angr-management-onefile-macos
 else
-    mv dist/angr-management dist/angr-management-ubuntu
+    cp onefile/angr-management upload/angr-management-onefile-ubuntu
 fi
-# Binary is currently at dist/start
+
+# Prepare onedirs
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    rm -rf dist/angr-management
+    hdiutil create upload/angr-management-macOS.dmg -volname "angr-management nightly" -srcfolder dist
+else
+    tar -C dist -czf upload/angr-management-ubuntu.tar.gz angr-management
+fi
---
+++ .azure-pipelines/bundle.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+# for finding various libs
+import angrmanagement
+import capstone
+import unicorn
+import pyvex
+import angr
+import cle
+import z3
+
+
+def make_common_options():
+    """
+    Create the pyinstaller command.
+    """
+
+    # any dynamically-loaded modules have to be explicitly added
+    included_data = [
+        (os.path.join(os.path.dirname(angrmanagement.__file__),
+                      "resources"), "angrmanagement/resources"),
+        (os.path.join(os.path.dirname(angrmanagement.__file__),
+                      "resources/images"), "angrmanagement/resources/images"),
+        (os.path.join(os.path.dirname(angrmanagement.__file__),
+                      "plugins"), "angrmanagement/plugins"),
+        (os.path.join(os.path.dirname(angrmanagement.__file__),
+                      "config"), "angrmanagement/config"),
+        (os.path.join(os.path.dirname(cle.__file__),
+                      "backends/pe/relocation"), "cle/backends/pe/relocation"),
+        (os.path.join(os.path.dirname(cle.__file__),
+                      "backends/elf/relocation"), "cle/backends/elf/relocation"),
+        (os.path.join(os.path.dirname(angr.__file__),
+                      "analyses/identifier/functions"), "angr/analyses/identifier/functions"),
+        (os.path.join(os.path.dirname(angr.__file__), "procedures"), "angr/procedures"),
+    ]
+
+    # dynamically-loaded DLLs have to be explicitly added. We just include the entire lib dir.
+    included_libs = [
+        (os.path.join(os.path.dirname(pyvex.__file__), "lib"), "pyvex/lib"),
+        (os.path.join(os.path.dirname(unicorn.__file__), "lib"), "unicorn/lib"),
+        (capstone._path, "capstone/lib"),
+        (os.path.join(os.path.dirname(z3.__file__), "lib"), "z3/lib"),
+    ]
+
+    if sys.platform == "linux":
+        import keystone
+        included_libs.append((os.path.dirname(keystone.__file__), "keystone"))
+
+    all_mappings = [(';' if sys.platform.startswith('win') else ':').join(
+        mapping) for mapping in (included_data + included_libs)]
+
+    # include ipython because it's not autodetected for some reason
+    args = ["pyinstaller",
+            "--name=angr-management",
+            "--hidden-import=ipykernel.datapub",
+            "-w",
+            "-i",
+            os.path.join(os.path.dirname(angrmanagement.__file__),
+                         "resources", "images", "angr.ico"),
+            ]
+
+    for mapping in all_mappings:
+        args.append("--add-data")
+        args.append(mapping)
+    args.append("start.py")
+
+    return args
+
+
+def make_bundle(onefile=True):
+    """
+    Execute the pyinstaller command.
+    """
+    args = make_common_options()
+
+    if onefile:
+        args.append("--onefile")
+        args.append("--distpath")
+        args.append("onefile")
+
+    if sys.platform in ("linux", "win32", "darwin"):
+        print(f"Creating bundle for {sys.platform}")
+        os.system(" ".join(args))
+    else:
+        print(f"Unsupported platform: {sys.platform}")
+
+
+def main():
+    if "--onefile" in sys.argv:
+        make_bundle(onefile=True)
+    if "--onedir" in sys.argv:
+        make_bundle(onefile=False)
+
+
+if __name__ == "__main__":
+    main()
