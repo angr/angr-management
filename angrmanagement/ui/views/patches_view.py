f76a92493a980265ca195132d4e9afa42c3c5a60
Fish <fish@cs.ucsb.edu>
2020-07-27 17:29:57 -0700

    Patches: Add apply patches button.

--- angrmanagement/ui/views/patches_view.py
+++ angrmanagement/ui/views/patches_view.py
@@ -1,5 +1,5 @@
 
-from PySide2.QtWidgets import QVBoxLayout
+from PySide2.QtWidgets import QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog
 
 from .view import BaseView
 from ..widgets.qpatch_table import QPatchTable
@@ -23,8 +23,40 @@ class PatchesView(BaseView):
 
     def _init_widgets(self):
 
+        # patch table
         self._patch_table = QPatchTable(self.workspace.instance, self)
 
-        layout = QVBoxLayout(self)
+        #
+        # controls
+        #
+        control_layout = QHBoxLayout()
+
+        # apply patches
+        btn_apply_patches = QPushButton("Apply patches to...")
+        btn_apply_patches.clicked.connect(self._on_apply_patches_clicked)
+        control_layout.addStretch(0)
+        control_layout.addWidget(btn_apply_patches)
+
+        layout = QVBoxLayout()
         layout.addWidget(self._patch_table)
+        layout.addLayout(control_layout)
         self.setLayout(layout)
+
+    #
+    # Events
+    #
+
+    def _on_apply_patches_clicked(self):
+
+        # select where the new binary will be stored
+        # Open File window
+        file_path, _ = QFileDialog.getSaveFileName(
+            self, "Save the patched binary to...",
+            self.workspace.instance.project.loader.main_object.binary + ".patched",  # FIXME: this will not work if we are loading from an angrdb
+            "Any file (*)",
+        )
+
+        if file_path:
+            b = self.workspace.instance.project.kb.patches.apply_patches_to_binary()
+            with open(file_path, "wb") as f:
+                f.write(b)
