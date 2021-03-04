4a69ce5c5ef84a8b57c426800527962bba17b6a9
Audrey Dutcher <audrey@rhelmot.io>
2021-03-04 14:34:56 -0700

    Switch to real unittest testcases to make nose2 happy

--- tests/test_qaddress_input.py
+++ tests/test_qaddress_input.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import unittest
 
 from PySide2.QtTest import QTest
 
@@ -10,46 +12,46 @@ from angrmanagement.ui.widgets.qaddress_input import QAddressInput
 from common import setUp, test_location
 
 
+class TestQaddressInput(unittest.TestCase):
+    def setUp(self):
+        setUp()
+
+    def test_address_conversion(self):
+        main = MainWindow(show=False)
+        main.workspace.instance.project = load_shellcode(b'X', 'amd64')
+        main.workspace.instance.project.kb.functions.function(addr=0x1234, name='foo', create=True)
+
+        obj = QAddressInput(None, main.workspace)
+
+        obj.setText("")
+        QTest.keyClicks(obj, "4321")
+        self.assertEqual(obj.target, 0x4321)
+
+        obj.setText("")
+        QTest.keyClicks(obj, "foo")
+        self.assertEqual(obj.target, 0x1234)
+
+        obj.setText("")
+        QTest.keyClicks(obj, "12x3")
+        self.assertIsNone(obj.target)
 
 
-def test_address_conversion():
-    main = MainWindow(show=False)
-    main.workspace.instance.project = load_shellcode(b'X', 'amd64')
-    main.workspace.instance.project.kb.functions.function(addr=0x1234, name='foo', create=True)
+    def test_function_name(self):
+        proj = angr.Project(os.path.join(test_location, "x86_64", "fauxware"), auto_load_libs=False)
+        main = MainWindow(show=False)
+        main.workspace.instance.project = proj
 
-    obj = QAddressInput(None, main.workspace)
+        cfg = proj.analyses.CFG()
+        obj = QAddressInput(None, main.workspace)
 
-    obj.setText("")
-    QTest.keyClicks(obj, "4321")
-    assert obj.target == 0x4321
+        obj.setText("")
+        QTest.keyClicks(obj, "main")
+        self.assertEqual(obj.target, cfg.kb.functions['main'].addr)
 
-    obj.setText("")
-    QTest.keyClicks(obj, "foo")
-    assert obj.target == 0x1234
-
-    obj.setText("")
-    QTest.keyClicks(obj, "12x3")
-    assert obj.target is None
-
-
-def test_function_name():
-    proj = angr.Project(os.path.join(test_location, "x86_64", "fauxware"), auto_load_libs=False)
-    main = MainWindow(show=False)
-    main.workspace.instance.project = proj
-
-    cfg = proj.analyses.CFG()
-    obj = QAddressInput(None, main.workspace)
-
-    obj.setText("")
-    QTest.keyClicks(obj, "main")
-    assert obj.target == cfg.kb.functions['main'].addr
-
-    obj.setText("")
-    QTest.keyClicks(obj, "main_1")
-    assert obj.target is None
+        obj.setText("")
+        QTest.keyClicks(obj, "main_1")
+        self.assertIsNone(obj.target)
 
 
 if __name__ == "__main__":
-    setUp()
-    test_address_conversion()
-    test_function_name()
+    unittest.main(argv=sys.argv)
