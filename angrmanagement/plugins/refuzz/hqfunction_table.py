857abdba465439658413978f18e109883c4a3d8d
novafacing <rowanbhart@gmail.com>
2021-03-05 17:02:10 -0500

    Add Refuzz Plugin

---
+++ angrmanagement/plugins/refuzz/hqfunction_table.py
@@ -0,0 +1,399 @@
+import os
+import string
+
+from angr.analyses.code_tagging import CodeTags
+
+from PySide2.QtWidgets import (
+    QWidget,
+    QTableView,
+    QAbstractItemView,
+    QHeaderView,
+    QVBoxLayout,
+    QLineEdit,
+    QStyledItemDelegate,
+)
+from PySide2.QtGui import QBrush, QColor
+from PySide2.QtCore import Qt, QSize, QAbstractTableModel, SIGNAL, QEvent
+
+from ...data.instance import ObjectContainer
+from ...config import Conf
+
+
+class QHFunctionTableModel(QAbstractTableModel):
+
+    Headers = ["Name", "Tags", "Address", "Binary", "Size", "Blocks"]
+    NAME_COL = 0
+    TAGS_COL = 1
+    ADDRESS_COL = 2
+    BINARY_COL = 3
+    SIZE_COL = 4
+    BLOCKS_COL = 5
+
+    def __init__(self, workspace, func_list, plugin):
+
+        super(QHFunctionTableModel, self).__init__()
+
+        self._func_list = None
+        self._raw_func_list = func_list
+        self.workspace = workspace
+        self.plugin = plugin
+
+    def __len__(self):
+        if self._func_list is not None:
+            return len(self._func_list)
+        if self._raw_func_list is not None:
+            return len(self._raw_func_list)
+        return 0
+
+    @property
+    def func_list(self):
+        if self._func_list is not None:
+            return self._func_list
+        return self._raw_func_list
+
+    @func_list.setter
+    def func_list(self, v):
+        self._func_list = None
+        self._raw_func_list = v
+        self.emit(SIGNAL("layoutChanged()"))
+
+    def filter(self, keyword):
+        if not keyword or self._raw_func_list is None:
+            # remove the filtering
+            self._func_list = None
+        else:
+            extra_columns = self.workspace.plugins.count_func_columns()
+            self._func_list = [
+                func
+                for func in self._raw_func_list
+                if self._func_match_keyword(func, keyword, extra_columns=extra_columns)
+            ]
+
+        self.emit(SIGNAL("layoutChanged()"))
+
+    def rowCount(self, *args, **kwargs):
+        if self.func_list is None:
+            return 0
+        return len(self.func_list)
+
+    def columnCount(self, *args, **kwargs):
+        return len(self.Headers) + self.workspace.plugins.count_func_columns()
+
+    def headerData(self, section, orientation, role):
+        if role != Qt.DisplayRole:
+            return None
+
+        if section < len(self.Headers):
+            return self.Headers[section]
+        else:
+            try:
+                return self.workspace.plugins.get_func_column(
+                    section - len(self.Headers)
+                )
+            except IndexError:
+                # Not enough columns
+                return None
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+
+        row = index.row()
+        if row >= len(self):
+            return None
+
+        col = index.column()
+        func = self.func_list[row]
+
+        if role == Qt.DisplayRole:
+            return self._get_column_text(func, col)
+
+        elif role == Qt.ForegroundRole:
+            color = QColor(0, 0, 0)
+            if func.is_syscall:
+                color = QColor(0, 0, 0x80)
+            elif func.is_plt:
+                color = QColor(0, 0x80, 0)
+            elif func.is_simprocedure:
+                color = QColor(0x80, 0, 0)
+            elif func.alignment:
+                color = Qt.darkMagenta
+
+            # for w in widgets:
+            #    w.setFlags(w.flags() & ~Qt.ItemIsEditable)
+            #    w.setForeground(color)
+
+            return QBrush(color)
+
+        elif role == Qt.BackgroundColorRole:
+            color = self.workspace.plugins.color_func(func)
+            if color is None:
+                color = QColor(0xFF, 0xFF, 0xFF)
+
+            if func.addr in self.plugin.suggested:
+                color = QColor(159, 237, 228)
+
+            return QBrush(color)
+
+        elif role == Qt.FontRole:
+            return Conf.tabular_view_font
+
+    def sort(self, column, order):
+        self.layoutAboutToBeChanged.emit()
+        self.func_list = sorted(
+            self.func_list,
+            key=lambda f: self._get_column_data(f, column),
+            reverse=order == Qt.DescendingOrder,
+        )
+        self.layoutChanged.emit()
+
+    #
+    # Private methods
+    #
+
+    def _get_column_data(self, func, idx):
+        if idx == self.NAME_COL:
+            return func.demangled_name
+        elif idx == self.TAGS_COL:
+            return func.tags
+        elif idx == self.ADDRESS_COL:
+            return func.addr
+        elif idx == self.BINARY_COL:
+            return self._get_binary_name(func)
+        elif idx == self.SIZE_COL:
+            return func.size
+        elif idx == self.BLOCKS_COL:
+            return len(func.block_addrs_set)
+        else:
+            return self.workspace.plugins.extract_func_column(
+                func, idx - len(self.Headers)
+            )[0]
+
+    def _get_column_text(self, func, idx):
+        if idx < len(self.Headers):
+            data = self._get_column_data(func, idx)
+            if idx == self.ADDRESS_COL:
+                return hex(data)
+            elif idx == self.TAGS_COL:
+                return self._get_tags_display_string(data)
+            else:
+                return str(data)
+
+        return self.workspace.plugins.extract_func_column(
+            func, idx - len(self.Headers)
+        )[1]
+
+    def _get_binary_name(self, func):
+        return os.path.basename(func.binary.binary) if func.binary is not None else ""
+
+    def _get_function_backcolor(self, func) -> QColor:
+        return self._backcolor_callback(func)
+
+    TAG_STRS = {
+        CodeTags.HAS_XOR: "Xor",
+        CodeTags.HAS_BITSHIFTS: "Shift",
+        CodeTags.HAS_SQL: "SQL",
+    }
+
+    @classmethod
+    def _get_tags_display_string(cls, tags):
+        return ", ".join(cls.TAG_STRS.get(t, t) for t in tags)
+
+    def _func_match_keyword(self, func, keyword, extra_columns: int = 0):
+        """
+        Check whether the function matches against the given keyword or not.
+
+        :param func:        The function to match on.
+        :param str keyword: The keyword to match against.
+        :return:            True if the function matches the keyword, False otherwise.
+        :rtype:             bool
+        """
+
+        keyword = keyword.lower()
+
+        if keyword in func.name.lower():
+            return True
+        demangled_name = func.demangled_name
+        if demangled_name and keyword in demangled_name.lower():
+            return True
+        if type(func.addr) is int:
+            if keyword in "%x" % func.addr:
+                return True
+            if keyword in "%#x" % func.addr:
+                return True
+        if keyword in ",".join(func.tags).lower():
+            return True
+        if func.binary and keyword in func.binary.binary.lower():
+            return True
+        if extra_columns > 0:
+            for idx in range(extra_columns):
+                txt = self.workspace.plugins.extract_func_column(func, idx)[1]
+                if txt and keyword in txt.lower():
+                    return True
+        return False
+
+
+class QHFunctionTableView(QTableView):
+    def __init__(self, parent, workspace, selection_callback=None, plugin=None):
+        super(QHFunctionTableView, self).__init__(parent)
+        self.workspace = workspace
+        self.plugin = plugin
+
+        self._function_table = parent  # type: QFunctionTable
+        self._selected_func = ObjectContainer(None, "Currently selected function")
+        self._selected_func.am_subscribe(selection_callback)
+
+        self.horizontalHeader().setVisible(True)
+        self.verticalHeader().setVisible(False)
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setHorizontalScrollMode(self.ScrollPerPixel)
+        self.horizontalHeader().setDefaultAlignment(Qt.AlignLeft)
+
+        # sorting
+        # self.horizontalHeader().setSortIndicatorShown(True)
+
+        self.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+        self.verticalHeader().setDefaultSectionSize(24)
+
+        self.show_alignment_functions = False
+        self._functions = None
+        self._model = QHFunctionTableModel(self.workspace, [], self.plugin)
+
+        self.setModel(self._model)
+
+        # slots
+        self.horizontalHeader().sortIndicatorChanged.connect(self.sortByColumn)
+        self.doubleClicked.connect(self._on_function_selected)
+
+    def refresh(self):
+        self.viewport().update()
+
+    @property
+    def function_manager(self):
+        return self._functions
+
+    @function_manager.setter
+    def function_manager(self, functions):
+        self._functions = functions
+        self.load_functions()
+
+    def subscribe_func_select(self, callback):
+        self._selected_func.am_subscribe(callback)
+
+    def filter(self, keyword):
+        self._model.filter(keyword)
+
+    def load_functions(self):
+        if not self.show_alignment_functions:
+            self._model.func_list = [
+                v for v in self._functions.values() if not v.alignment
+            ]
+        else:
+            self._model.func_list = list(self._functions.values())
+
+    def _on_function_selected(self, model_index):
+        row = model_index.row()
+        self._selected_func.am_obj = self._model.func_list[row]
+        self._selected_func.am_event(func=self._selected_func.am_obj)
+
+    def keyPressEvent(self, key_event):
+
+        text = key_event.text()
+        if not text or text not in string.printable or text in string.whitespace:
+            # modifier keys
+            return super(QHFunctionTableView, self).keyPressEvent(key_event)
+
+        # show the filtering text box
+        self._function_table.show_filter_box(prefix=text)
+        return True
+
+
+class QHFunctionTable(QWidget):
+    def __init__(self, parent, workspace, selection_callback=None, plugin=None):
+        super(QHFunctionTable, self).__init__(parent)
+        self.workspace = workspace
+        self.plugin = plugin
+
+        self._view = parent  # type: 'FunctionsView'
+        self._table_view = None  # type: QHFunctionTableView
+        self._filter_box = None  # type: QFunctionTableFilterBox
+        self._toolbar = None  # type: FunctionTableToolbar
+
+        self._init_widgets(selection_callback)
+
+    @property
+    def show_alignment_functions(self):
+        return self._table_view.show_alignment_functions
+
+    @property
+    def function_manager(self):
+        if self._table_view is not None:
+            return self._table_view.function_manager
+        return None
+
+    @function_manager.setter
+    def function_manager(self, v):
+        if v is not None:
+            self._view.set_function_count(len(v))
+        if self._table_view is not None:
+            self._table_view.function_manager = v
+        else:
+            raise ValueError("QHFunctionTableView is uninitialized.")
+        self.update_displayed_function_count()
+
+    #
+    # Public methods
+    #
+
+    def refresh(self):
+        self._table_view.refresh()
+
+    def toggle_show_alignment_functions(self):
+        self._table_view.show_alignment_functions = (
+            not self._table_view.show_alignment_functions
+        )
+        self._table_view.load_functions()
+        self.update_displayed_function_count()
+
+    def subscribe_func_select(self, callback):
+        self._table_view.subscribe_func_select(callback)
+
+    def update_displayed_function_count(self):
+        cnt = self._table_view.model().rowCount()
+        if self.function_manager is None:
+            return
+        if cnt == len(self.function_manager):
+            self._view.set_displayed_function_count(None)  # no filtering
+        else:
+            self._view.set_displayed_function_count(cnt)
+
+    def filter_functions(self, text):
+        self._table_view.filter(text)
+        if not text:
+            self._view.set_displayed_function_count(None)
+        else:
+            self.update_displayed_function_count()
+
+    #
+    # Private methods
+    #
+
+    def _init_widgets(self, selection_callback=None):
+
+        # function table view
+        self._table_view = QHFunctionTableView(
+            self, self.workspace, selection_callback, self.plugin
+        )
+
+        # filter text box
+        # layout
+        layout = QVBoxLayout()
+        layout.addWidget(self._table_view)
+
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(layout)
+
+    #
+    # Events
+    #
--- angrmanagement/plugins/refuzz/refuzz_plugin.py
+++ angrmanagement/plugins/refuzz/refuzz_plugin.py
@@ -1,21 +1,48 @@
 from ..base_plugin import BasePlugin
+from ...data.object_container import ObjectContainer
 
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QAbstractTableModel
 from PySide2.QtGui import QColor
-from PySide2.QtWidgets import QApplication, QInputDialog, QMessageBox, QVBoxLayout, QPushButton
+from PySide2.QtWidgets import (
+    QApplication,
+    QInputDialog,
+    QMessageBox,
+    QVBoxLayout,
+    QAbstractItemView,
+    QPushButton,
+    QHeaderView,
+    QLineEdit,
+    QLabel,
+)
 from angrmanagement.ui.views import BaseView
 from angrmanagement.ui.widgets.qfunction_combobox import QFunctionComboBox
-from angrmanagement.ui.widgets.qfunction_table import QFunctionTableView, QFunctionTableModel, QFunctionTable
+from angr.analyses.decompiler.decompilation_options import (
+    DecompilationOption,
+    options as dec_options,
+)
+from angr.analyses.decompiler.optimization_passes import (
+    get_default_optimization_passes,
+    get_optimization_passes,
+)
+from angr.analyses.decompiler.peephole_optimizations import EXPR_OPTS, STMT_OPTS
+from .hqfunction_table import *
+import json
+import requests
+import base64
+import asyncio
+import time
+from pathlib import Path
+
 
 class ReFuzzView(BaseView):
     def __init__(self, workspace, default_docking_position, *args, **kwargs):
-        if 'plugin' not in kwargs:
+        if "plugin" not in kwargs:
             return
         else:
-            self.plugin = kwargs['plugin']
-            del kwargs['plugin']
+            self.plugin = kwargs["plugin"]
+            del kwargs["plugin"]
 
-        super().__init__('refuzz', workspace, default_docking_position, *args, **kwargs)
+        super().__init__("refuzz", workspace, default_docking_position, *args, **kwargs)
 
         self.caption = "ReFuzz"
         self.category = "Patching"
@@ -24,31 +51,95 @@ class ReFuzzView(BaseView):
 
     def _init_widgets(self):
         layout = QVBoxLayout()
-        self.get_suggestions_button = QPushButton("Get Patch Suggestions")
+        self.get_suggestions_button = QPushButton("Get Reinterface Suggestions")
         self.get_suggestions_button.clicked.connect(self.plugin.get_patch_suggestions)
-        self.patch_function_button = QPushButton("Patch Selected Function")
+        self.patch_function_button = QPushButton("Reinterface Selected Function")
         self.patch_function_button.clicked.connect(self.plugin.patch_function)
-        self.patch_function_selector = QFunctionTable(self, workspace=self.plugin.workspace, selection_callback=self.plugin.set_selected_function)
-        #self.patch_function_selector_view = QFunctionTableView(parent=self.patch_function_selector, workspace=self.plugin.workspace, selection_callback=self.plugin.set_selected_function)
+        self.refresh_button = QPushButton("Refresh Listing")
+        self.refresh_button.clicked.connect(self.refresh_listing)
+        self.patch_server_url_entry = QLineEdit()
+        self.patch_server_url_entry.setPlaceholderText("Patch Server URL")
+        self.patch_server_url_entry.textChanged.connect(self.set_patch_server_url_info)
+        self.server_url_entry = QLineEdit()
+        self.server_url_entry.setPlaceholderText("Analysis Server URL")
+        self.server_url_entry.textChanged.connect(self.set_server_url_info)
+        # {"func_addr":"0x400827","buf_addr":"arg_sym_reg_rdx","buf_outlen_addr":"arg_sym_reg_rcx;","buf_maxlen":"-1"}%
+        self.buffer_addr_box = QLabel()
+        self.buffer_outlen_addr_box = QLabel()
+        self.buffer_maxlen_box = QLabel()
+
+        self.patch_function_selector = QHFunctionTable(
+            self,
+            workspace=self.plugin.workspace,
+            selection_callback=self.plugin.set_selected_function,
+            plugin=self.plugin,
+        )
+        # self.patch_function_selector_view = QFunctionTableView(parent=self.patch_function_selector, workspace=self.plugin.workspace, selection_callback=self.plugin.set_selected_function)
+        layout.addWidget(self.server_url_entry)
+        layout.addWidget(self.patch_server_url_entry)
         layout.addWidget(self.get_suggestions_button)
         layout.addWidget(self.patch_function_button)
+        layout.addWidget(self.refresh_button)
         layout.addWidget(self.patch_function_selector)
+        layout.addWidget(self.buffer_addr_box)
+        layout.addWidget(self.buffer_outlen_addr_box)
+        layout.addWidget(self.buffer_maxlen_box)
         self.setLayout(layout)
+
+    def set_function_count(self, i):
+        pass
+
+    def set_displayed_function_count(self, i):
+        pass
+
+    def set_server_url_info(self, text):
+        self.plugin.set_server_url(text)
+
+    def set_patch_server_url_info(self, text):
+        self.plugin.set_patch_server_url(text)
+
+    def refresh_listing(self):
+        if self.patch_function_selector:
+            if self.plugin.workspace.instance.cfg is not None:
+                self.patch_function_selector.function_manager = (
+                    self.plugin.workspace.instance.kb.functions
+                )
+                # self._function_table.refresh()
+        self.buffer_addr_box.setText(self.plugin.buffer_addr)
+        self.buffer_maxlen_box.setText(self.plugin.buffer_maxlen)
+        self.buffer_outlen_addr_box.setText(self.plugin.buffer_outlen)
+
 
 class RefuzzPlugin(BasePlugin):
     REQUIRE_WORKSPACE = True
-    MENU_BUTTONS = ('Get Patch Suggestions', 'Patch Function')
+    MENU_BUTTONS = ("Get Patch Suggestions", "Patch Function")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.workspace.instance.register_container('refuzz', lambda: None, None, 'Refuzz Control Panel')
-        self.view = ReFuzzView(plugin=self, workspace=self.workspace, default_docking_position='center')
+        self.workspace.instance.register_container(
+            "refuzz", lambda: None, None, "Refuzz Control Panel"
+        )
+        self.view = ReFuzzView(
+            plugin=self, workspace=self.workspace, default_docking_position="center"
+        )
         self.workspace.add_view(self.view, self.view.caption, self.view.category)
-        self.handlers = {
-            0: self.get_patch_suggestions,
-            1: self.patch_function
-        }
+        self.handlers = {0: self.get_patch_suggestions, 1: self.patch_function}
+        self.suggested = []
+        self.session = requests.Session()
+        self.server_url = ""
+        self.patch_server_url = ""
+        self.checking = False
+        self.selected_function = 0
+        self.buffer_addr = ""
+        self.buffer_maxlen = ""
+        self.buffer_outlen = ""
+
+    def set_patch_server_url(self, url):
+        self.patch_server_url = url
+
+    def set_server_url(self, url):
+        self.server_url = url
 
     def handle_click_menu(self, idx):
         if idx < 0 or idx > len(self.buttons):
@@ -60,11 +151,116 @@ class RefuzzPlugin(BasePlugin):
         self.handlers[idx]()
 
     def get_patch_suggestions(self):
-        pass
+        query_functions = {}
+        if self.workspace.instance.kb is None:
+            print("Err: Must load a binary.")
+            return
 
+        if self.server_url == "":
+            print("Err: Must specify url")
+            return
+
+        if self.checking:
+            print("Err: currently checking for suggestions. Cancel or wait.")
+
+        opts = list(map(lambda o: (o, True), dec_options))
+        peephole_opts = STMT_OPTS + EXPR_OPTS
+        passes = get_optimization_passes(
+            self.workspace.instance.project.arch,
+            self.workspace.instance.project.simos.name,
+        )
+        print(opts)
+        for addr in self.workspace.instance.kb.functions:
+            func = self.workspace.instance.kb.functions[addr]
+            """
+            if (
+                func.prototype is not None
+                and not func.is_syscall
+                and not func.is_simprocedure
+            ):
+            """
+            """
+            try:
+                d = self.workspace.instance.project.analyses.Decompiler(
+                    func,
+                    cfg=self.workspace.instance.cfg,
+                    options=opts,
+                    optimization_passes=passes,
+                    peephole_optimizations=peephole_opts,
+                )
+                code = d.codegen.text
+                proto = code[0 : code.find("{")].rstrip()
+                print(proto)
+                print(f"Decompiled function at {hex(func.addr)}")
+
+                query_functions[hex(func.addr)] = proto
+            except Exception as e:
+                print(f"Could not decompile function at {hex(func.addr)}: {e}")
+            """
+
+        with open(self.workspace.instance.project.filename, "rb") as binfile:
+            contents = binfile.read()
+            status = self.session.get(f"{self.server_url}/status")
+            # js_status = json.loads(status.text)
+            self.session.get(f"{self.server_url}/stop")
+
+            res = self.session.get(
+                f"{self.server_url}/start?{Path(self.workspace.instance.project.filename).name}"
+            )
+
+            """
+            res = self.session.post(
+                f"{self.server_url}/start?{Path(self.workspace.instance.project.filename).name}",
+                json={
+                    "functions": query_functions,
+                    "binary": base64.b64encode(contents).decode("UTF-8"),
+                },
+            )
+            """
+            self.start_checker()
+
+    def start_checker(self):
+        self.checking = True
+        loop = asyncio.get_event_loop()
+        loop.run_in_executor(None, self.checker_thread)
+
+    def checker_thread(self):
+        while True:
+            res = self.session.get(f"{self.server_url}/status")
+            print(res.text)
+            # js_res = json.loads(res.text)
+            if res is not None and "finished" in res.text:
+                req = f"{self.server_url}/result?{Path(self.workspace.instance.project.filename).name}"
+                print(f"Requesting {req}")
+                fres = self.session.get(req)
+                print(fres.text)
+                js_fres = json.loads(fres.text)
+                self.last_result = js_fres
+                print(self.last_result)
+                self.suggested = [int(self.last_result["func_addr"], 16)]
+                # {"func_addr":"0x400827","buf_addr":"arg_sym_reg_rdx","buf_outlen_addr":"arg_sym_reg_rcx;","buf_maxlen":"-1"}%
+                self.buffer_addr = self.last_result["buf_addr"]
+                self.buffer_outlen = self.last_result["buf_outlen_addr"]
+                self.buffer_maxlen = self.last_result["buf_maxlen"]
+                return
+            else:
+                print("No result available. Sleeping.")
+                time.sleep(10)
 
     def patch_function(self):
-        pass
+        f = {"bin": open(self.workspace.instance.project.filename, "rb")}
+        res = self.session.post(f"{self.patch_server_url}/analyze/", files=f)
+        res2 = self.session.get(
+            f"{self.patch_server_url}/run/?addr={int(self.selected_function, 16) - 0x400000}"
+        )
+        if res2 is not None:
+            js_res2 = json.loads(res2.text)
+            file_content = js_res2["file"]
+            real_content = base64.b64decode(file_content)
+            with open(
+                f"{self.workspace.instance.project.filename}-patched", "wb"
+            ) as patchfile:
+                patchfile.write(real_content)
 
-    def set_selected_function(self):
-        pass
+    def set_selected_function(self, func):
+        self.selected_function = hex(func.addr)
