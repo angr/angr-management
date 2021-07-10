3d472c7287b921fa1630cfa37bf7012475a2fb7a
Tiffany Bao <tiffany.yzh@gmail.com>
2021-07-10 16:59:57 -0400

    remove timestamp since slacrs will add it by default

--- angrmanagement/plugins/log_human_activities/log_human_activities.py
+++ angrmanagement/plugins/log_human_activities/log_human_activities.py
@@ -1,5 +1,6 @@
-from ..base_plugin import BasePlugin
 import logging
+import hashlib
+from ..base_plugin import BasePlugin
 
 l = logging.getLogger(__name__)
 l.setLevel('DEBUG')
@@ -24,6 +25,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
         super().__init__(*args, **kwargs)
         self.session = Slacrs().session()
         self.project_name = None
+        self.project_md5 = None
 
     def handle_variable_rename(self, func, offset: int, old_name: str, new_name: str):
         """
@@ -31,11 +33,14 @@ class LogHumanActivitiesPlugin(BasePlugin):
         """
         variable_rename = HumanActivityVariableRename(
             project=self.project_name,
+            project_md5=self.project_md5,
             function=func._name,
             old_name=old_name,
             new_name=new_name,
-            created_by=TODO)
+            created_by=TODO,
+        )
         self.session.add(variable_rename)
+        self.session.commit()
         l.info("Add variable rename sesssion to slacrs")
 
     def handle_function_rename(self, func, old_name: str, new_name: str):
@@ -44,11 +49,16 @@ class LogHumanActivitiesPlugin(BasePlugin):
         """
         function_rename = HumanActivityFunctionRename(
             project=self.project_name,
+            project_md5=self.project_md5,
             old_name=old_name,
             new_name=new_name,
-            created_by=TODO)
+            created_by=TODO,
+        )
         self.session.add(function_rename)
-        l.info("Add function rename sesssion to slacrs")
+        self.session.commit()
+        l.info("Add function rename sesssion to slacrs, project name %s, old_name %s, new_name %s", self.project_name, old_name, new_name)
+        result = self.session.query(HumanActivityFunctionRename).filter(HumanActivityFunctionRename.project == self.project_name).first()
+        l.info("Query result: old_name %s, new_name %s", result.old_name, result.new_name)
 
     def handle_project_initialization(self):
         """
@@ -57,8 +67,12 @@ class LogHumanActivitiesPlugin(BasePlugin):
         if self.workspace.instance.img_name is not None:
             self.project_name = self.workspace.instance.img_name
         else:
-            self.project_name = self.workspace.instance.project.filename
-        l.info("Set project name")
+            filename = self.workspace.instance.project.filename
+            self.project_name = filename
+            with open(filename, 'rb') as f:
+                self.project_md5 = hashlib.md5(f.read()).hexdigest()
+            l.info("Set project md5 to %s", self.project_md5)
+        l.info("Set project name to %s", self.project_name)
 
     def teardown(self):
         self.session.close()
