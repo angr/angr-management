d636ba269962acd4fe4eb02f7b5749676e02ab0e
Zeming YU <yuzeming@gmail.com>
2021-07-15 16:28:45 +0800

    Add two plugins: `ExecutionStatisticsViewer` and `SourceViewerPlugin` (#393)
    * add hci-plugins
    
    * clean up
    
    * fix linting
    
    * fix linting
    
    * add docstring and remove ref of view

---
+++ angrmanagement/plugins/execution_statistics_viewer/execution_statistics_viewer.py
@@ -0,0 +1,120 @@
+from collections import defaultdict
+from typing import TYPE_CHECKING
+from PySide2.QtWidgets import QLabel
+
+from angrmanagement.plugins.base_plugin import BasePlugin
+from angrmanagement.logic.threads import gui_thread_schedule
+from angrmanagement.ui.widgets.qinst_annotation import QActiveCount, QPassthroughCount
+
+if TYPE_CHECKING:
+    from angrmanagement.ui.views import DisassemblyView, SymexecView
+
+
+class ExecutionStatisticsViewer(BasePlugin):
+    """
+    Add a step_callback hook to count how many state on and passthrough of a particular address.
+    return the result in build_qblock_annotations callback and show in the disassmbly view by
+    fetch_qblock_annotations function.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.passthrough_counts = defaultdict(int)
+        self.addr_to_active_states = defaultdict(list)
+        self.returning_to_here_states = defaultdict(list)
+        self.bb_addrs = None
+        self.instance = self.workspace.instance
+
+        if self.symexec_view:
+            self.current_simgr = self.symexec_view.current_simgr
+            self.current_simgr.am_subscribe(self._on_simgr_selected)
+        self._init_widgets()
+
+    @property
+    def disasm_view(self) -> 'DisassemblyView':
+        return self.workspace.view_manager.first_view_in_category("disassembly")
+
+    @property
+    def symexec_view(self) -> 'SymexecView':
+        return self.workspace.view_manager.first_view_in_category("symexec")
+
+    def count_passthrough(self, simgr):
+        """Prior to stepping the active states, increment the passthrough count on the basic block(s) that will be
+        executed next."""
+        if self.bb_addrs is None:
+            self.bb_addrs = set(b.addr for b in self.instance.cfg.nodes())
+        for s in simgr.active:
+            for i_addr in s.block().instruction_addrs:
+                if i_addr in self.bb_addrs:
+                    self.passthrough_counts[i_addr] += 1
+
+    def step_callback(self, simgr):
+        """Called after stepping the simgr"""
+        self.count_passthrough(simgr)
+        self.count_active_states(simgr)
+        gui_thread_schedule(self._refresh_gui)
+
+    def count_active_states(self, simgr):
+        """Count the number of states at each instruction in the program. Do some special work to figure out how many
+        states are under each call and syscall instruction."""
+        self.addr_to_active_states = defaultdict(list)
+        self.returning_to_here_states = defaultdict(list)
+        for s in simgr.active:
+            # Count states at instruction
+            self.addr_to_active_states[s.addr].append(s)
+            # Count states under calls
+            stack_frame = s.callstack
+            while stack_frame:
+                self.returning_to_here_states[stack_frame.ret_addr].append(s)
+                stack_frame = stack_frame.next
+            # Count states in syscalls
+            if s.history.jumpkind.startswith("Ijk_Sys"):
+                self.addr_to_active_states[s.history.jump_source].append(s)
+
+    def _on_simgr_selected(self, *args, src=None, **kwargs): #pylint: disable=unused-argument
+        """Listener for when a new simgr is selected in the symexec view"""
+        if src in ["clicked", "from above"]:
+            # The "from above" event is emitted when you create a new simgr via right click menu in disasm view
+            self.passthrough_counts = defaultdict(int)
+            self.addr_to_active_states = defaultdict(list)
+            self.returning_to_here_states = defaultdict(list)
+            if not self.current_simgr.am_none:
+                self.count_active_states(self.current_simgr)
+            self._refresh_gui()
+
+    def update_active_states_label(self):
+        if not self.current_simgr.am_none:
+            self.active_states_label.setText(f"Active states: {len(self.current_simgr.active)}")
+
+    def build_qblock_annotations(self, qblock):
+        if self.current_simgr.am_none:
+            # If there's no simgr selected at this moment, then there arent' any labels to show? Maybe
+            return []
+
+        qinsns = qblock.addr_to_insns.values()
+
+        items = []
+        for qinsn in qinsns:
+            addr = qinsn.addr
+            active_states = []
+            passthrough_count = 0
+            active_states += self.addr_to_active_states[addr]
+            passthrough_count += self.passthrough_counts[addr]
+            if qinsn.insn.mnemonic.opcode_string == "call":
+                ret_addr = qinsn.insn.addr + qinsn.insn.size
+                active_states += self.returning_to_here_states[ret_addr]
+            if len(active_states) > 0:
+                items.append(QActiveCount(qinsn.addr, active_states))
+            if passthrough_count > 0:
+                items.append(QPassthroughCount(qinsn.addr, passthrough_count))
+        return items
+
+    def _refresh_gui(self):
+        self.update_active_states_label()
+        self.disasm_view.refresh()
+
+    def _init_widgets(self):
+        statusbar_layout = self.disasm_view._statusbar.layout()
+        label = QLabel("Active states: 0")
+        label.setStyleSheet("QLabel {color: green}")
+        statusbar_layout.insertWidget(1, label)
+        self.active_states_label = label
---
+++ angrmanagement/plugins/source_viewer/source_viewer_plugin.py
@@ -0,0 +1,236 @@
+from collections import defaultdict
+from typing import TYPE_CHECKING
+from sortedcontainers import SortedDict
+
+from PySide2.QtGui import QCursor
+from PySide2.QtWidgets import QMenu,QVBoxLayout
+
+from pyqodeng.core.api import CodeEdit
+from pyqodeng.core.panels import LineNumberPanel, MarkerPanel, Marker
+from pyqodeng.core.widgets import SplittableCodeEditTabWidget
+from pyqodeng.core.api.panel import Panel
+from pyqodeng.core.api.utils import drift_color
+
+from qtpy import QtCore, QtGui
+from cle import Loader
+
+from angrmanagement.ui.views import BaseView
+from angrmanagement.plugins import BasePlugin
+from angrmanagement.ui.workspace import Workspace
+
+if TYPE_CHECKING:
+    from angrmanagement.ui.views.disassembly_view import DisassemblyView
+    from angrmanagement.ui.views.symexec_view import SymexecView
+
+class VaildLineNumberPanel(LineNumberPanel):
+    """
+    The color of line numbers indicates if any address associated with this line.
+    The vaild lineno are black, which means we could set a breakpoint on it and
+    the invaild are grey.
+    """
+    def __init__(self, valid_line=None):
+        super().__init__()
+        self._valid_line = valid_line or set()
+
+    def paintEvent(self, event):
+        # Paints the line numbers
+        self._line_color_u = drift_color(self._background_brush.color(), 250)
+        self._line_color_s = drift_color(self._background_brush.color(), 280)
+        Panel.paintEvent(self, event)
+        if not self.isVisible():
+            return
+        painter = QtGui.QPainter(self)
+        # get style options (font, size)
+        width = self.width()
+        height = self.editor.fontMetrics().height()
+        font = self.editor.font()
+        bold_font = self.editor.font()
+        bold_font.setBold(True)
+        pen = QtGui.QPen(self._line_color_u)
+        pen_selected = QtGui.QPen(self._line_color_s)
+        painter.setFont(font)
+        # draw every visible blocks
+        for top, line, _block in self.editor.visible_blocks:
+            if line+1 in self._valid_line:
+                painter.setPen(pen_selected)
+                painter.setFont(bold_font)
+            else:
+                painter.setPen(pen)
+                painter.setFont(font)
+            painter.drawText(-3, top, width, height,
+                                QtCore.Qt.AlignRight, str(line + 1))
+
+
+class SourceCodeViewer(CodeEdit):
+    """
+    CodeEdit for one source code file.
+    Used by SourceCodeViewerTabWidget.
+    """
+    viewer = None # type: SourceViewer
+    current_line = -1 # type: int
+
+    def __init__(self, parent):
+        super().__init__(parent=parent, create_default_actions=False)
+        self.setReadOnly(True)
+        self._valid_line = None
+        self.linenumber_panel = VaildLineNumberPanel()
+        self.breakpoint_panel = MarkerPanel()
+        self.breakpoint_panel.add_marker_requested.connect(self.add_marker_fn)
+        self.breakpoint_panel.edit_marker_requested.connect(self.edit_marker_fn)
+        self.breakpoint_panel.remove_marker_requested.connect(self.remove_marker_fn)
+
+        #self.state_panel = MarkerPanel()
+
+        self.panels.append(self.linenumber_panel)
+        self.panels.append(self.breakpoint_panel)
+        #self.panels.append(self.state_panel)
+
+    def set_valid_line(self, valid_line):
+        self.linenumber_panel._valid_line = valid_line
+        self._valid_line = valid_line
+        self.update()
+
+    def updateState(self, state_counter):
+        self.state_panel.updateState(state_counter)
+
+    def add_find(self):
+        desc = self.viewer.add_point(self.file.path, self.current_line, "find")
+        self.breakpoint_panel.add_marker(Marker(self.current_line-1,"edit-find",desc))
+
+    def add_avoid(self,):
+        desc = self.viewer.add_point(self.file.path, self.current_line, "avoid")
+        self.breakpoint_panel.add_marker(Marker(self.current_line-1,"edit-delete",desc))
+
+    def add_marker_fn(self, line):
+        self.current_line = line + 1
+        if self.current_line not in self._valid_line:
+            return
+        menu = QMenu()
+        menu.addAction("Add Find", self.add_find)
+        menu.addAction("Add Avoid", self.add_avoid)
+        menu.exec_(QCursor.pos())
+
+    def remove_marker_fn(self, line):
+        self.current_line = line + 1
+        self.viewer.remove_point(self.file.path, self.current_line)
+        lst = self.breakpoint_panel.marker_for_line(line)
+        for m in lst:
+            self.breakpoint_panel.remove_marker(m)
+
+    def edit_marker_fn(self,line):
+        self.current_line = line + 1
+        menu = QMenu()
+        menu.addAction("Edit Condition", self.edit_cond)
+        menu.exec_(QCursor.pos())
+
+    def edit_cond(self):
+        pass
+
+
+class SourceCodeViewerTabWidget(SplittableCodeEditTabWidget):
+    """
+    CodeEdit for one ELF file.
+    It could contain more than one file.
+    """
+    editors = {mimetype: SourceCodeViewer for mimetype in SourceCodeViewer.mimetypes}
+    fallback_editor = SourceCodeViewer
+    addr_to_line = None # type: SortedDict
+    line_to_addr = None # type: dict
+    viewer = None # type: SourceViewer
+    tabs = {} # type: dict(str,SourceCodeViewer)
+
+    def __init__(self, parent=None, addr_to_line: SortedDict=None, viewer=None):
+        super().__init__(parent=parent)
+        self.viewer = viewer
+        if addr_to_line is not None:
+            self.load(addr_to_line)
+
+    def load(self, addr_to_line: SortedDict):
+        self.addr_to_line = addr_to_line # (filename, line.state.line)
+        self.line_to_addr = defaultdict(list)
+        self.file_list = set()
+        for addr,(filename,line) in self.addr_to_line.items():
+            self.file_list.add(filename)
+            self.line_to_addr[(filename,line)].append(addr)
+        for fn in self.file_list:
+            editor = self.open_document(fn) #type: SourceCodeViewer
+            editor.viewer = self.viewer
+            valid_line = {line for (filename,line) in self.line_to_addr.keys() if filename == fn }
+            editor.set_valid_line(valid_line)
+            self.tabs[fn] = editor
+
+class SourceViewer(BaseView):
+    """
+    Main class of the Source Viewer Plugin
+    """
+    addr_to_line = None # type: SortedDict
+
+    main = None # type: SourceCodeViewerTabWidget
+    def __init__(self, workspace :Workspace, *args, **kwargs):
+        super().__init__("SourceViewer", workspace, *args, **kwargs)
+        self.caption = "Source Viewer"
+        self.workspace = workspace
+        self.instance = workspace.instance
+        workspace.instance.project.am_subscribe(self.load_from_proejct)
+        self._init_widgets()
+
+    @property
+    def disasm_view(self) -> 'DisassemblyView':
+        return self.workspace.view_manager.first_view_in_category("disassembly")
+
+    @property
+    def symexec_view(self) -> 'SymexecView':
+        return self.workspace.view_manager.first_view_in_category('symexec')
+
+    def _init_widgets(self):
+        self.main = SourceCodeViewerTabWidget()
+        self.main.viewer = self
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.main)
+        self.setLayout(main_layout)
+
+    def load_from_proejct(self, **kwargs): #pylint: disable=unused-argument
+        if self.instance.project.am_none:
+            return
+        loader = self.instance.project.loader #type: Loader
+        if hasattr(loader.main_object,"addr_to_line") and \
+                loader.main_object.addr_to_line is not None:
+            self.main.load(loader.main_object.addr_to_line)
+
+    def add_point(self, fn, line, typ):
+        symexec_view = self.symexec_view
+        if not symexec_view:
+            return ""
+        address_list = self.main.line_to_addr[(fn,line)]
+
+        for addr in address_list:
+            if typ == "find":
+                symexec_view.find_addr_in_exec(addr)
+            else:
+                symexec_view.avoid_addr_in_exec(addr)
+        return "\n".join([ ("0x%x" % i) for i in  address_list])
+
+    def remove_point(self, fn, line):
+        symexec_view = self.symexec_view
+        if not symexec_view:
+            return
+
+        address_list = self.main.line_to_addr[(fn,line)]
+        for addr in address_list:
+            symexec_view.remove_find_addr_in_exec(addr)
+            symexec_view.remove_avoid_addr_in_exec(addr)
+
+
+class SourceViewerPlugin(BasePlugin):
+    """
+    Plugin loader
+    """
+    def __init__(self, workspace):
+        super().__init__(workspace)
+        self.source_viewer = SourceViewer(workspace, "center")
+        workspace.default_tabs += [self.source_viewer]
+        workspace.add_view(
+            self.source_viewer,
+            self.source_viewer.caption,
+            self.source_viewer.category,
+        )
