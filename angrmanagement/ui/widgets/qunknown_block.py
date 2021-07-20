2b2be26f3bd8a8f2dfd40a84a07dde9a9696e02f
Fish <fish@cs.ucsb.edu>
2021-07-20 10:45:40 -0700

    QUnknownBlock: Fix a corner case.

--- angrmanagement/ui/widgets/qunknown_block.py
+++ angrmanagement/ui/widgets/qunknown_block.py
@@ -62,7 +62,7 @@ class QUnknownBlock(QCachedGraphicsItem):
             for i, b in enumerate(self.bytes):
                 line += "%02x " % b
                 if i > 0 and (i + 1) % 16 == 0:
-                    self._bytes_text.append(line)
+                    self._byte_lines.append(line)
                     line = ""
 
             if line:
