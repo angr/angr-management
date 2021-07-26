8a30576deff26db692cb00e214e348450790a685
Kevin Phoenix <kevin@kphoenix.us>
2021-07-26 14:23:04 -0700

    Delay plugin log initialization to when object is created rather than import

--- angrmanagement/plugins/chess_manager/diagnose_handler.py
+++ angrmanagement/plugins/chess_manager/diagnose_handler.py
@@ -22,22 +22,14 @@ except ImportError as _:
 l = logging.getLogger(__name__)
 l.setLevel('INFO')
 
-user_dir = os.path.expanduser('~')
-log_dir = os.path.join(user_dir, 'am-logging')
-os.makedirs(log_dir, exist_ok=True)
-log_file = os.path.join(log_dir, 'poi_diagnose.log')
-fh = logging.FileHandler(log_file)
-fh.setLevel('INFO')
-formatter: Formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-l.addHandler(fh)
-
 
 class DiagnoseHandler:
     """
     Handling POI records in slacrs
     """
     def __init__(self, project_name=None, project_md5=None):
+        self._init_logger()
+
         self.project_name = project_name
         self.project_md5 = project_md5
 
@@ -50,6 +42,17 @@ class DiagnoseHandler:
             self._active = False
         else:
             self._active = True
+
+    def _init_logger(self):
+        user_dir = os.path.expanduser('~')
+        log_dir = os.path.join(user_dir, 'am-logging')
+        os.makedirs(log_dir, exist_ok=True)
+        log_file = os.path.join(log_dir, 'poi_diagnose.log')
+        fh = logging.FileHandler(log_file)
+        fh.setLevel('INFO')
+        formatter: Formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        l.addHandler(fh)
 
     def init(self, workspace):
         l.debug("workspace initing")
