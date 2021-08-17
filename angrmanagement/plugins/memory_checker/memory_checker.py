1b66cdf086ed8b636a29ed86c2f28bd51d8c4f0b
Zeming Yu <yuzeming@gmail.com>
2021-08-17 23:17:39 +0800

    Add stderr output

--- angrmanagement/plugins/memory_checker/memory_checker.py
+++ angrmanagement/plugins/memory_checker/memory_checker.py
@@ -2,7 +2,7 @@ from typing import List, TYPE_CHECKING
 from angr import options
 from angr.state_plugins.sim_action import SimAction
 from angr.state_plugins.heap import SimHeapPTMalloc
-from sortedcontainers.sortedlist import SortedList
+from sortedcontainers.sorteddict import SortedDict
 from angrmanagement.plugins.base_plugin import BasePlugin
 
 if TYPE_CHECKING:
@@ -29,15 +29,19 @@ class MemoryChecker(BasePlugin):
         return  state.solver.eval(ptr)
 
     @staticmethod
-    def check_address_is_free(state: 'SimState', ptr_list):
-        ptr_list = [MemoryChecker.eval_ptr(state, x) for x in ptr_list]
-        ptr_list = SortedList(ptr_list)
-        len_list = len(ptr_list)
+    def check_address_is_free(state: 'SimState', ptr_list: 'List[SimAction]'):
+        ptr_dict = [(MemoryChecker.eval_ptr(state, x.addr.ast), x) for x in ptr_list]
+        ptr_dict = SortedDict(ptr_dict)
+        len_list = len(ptr_dict)
         for chunk in state.heap.free_chunks():
             base = chunk.base
             size = state.solver.eval(chunk.get_size())
-            p = ptr_list.bisect_left(base)
-            if p <  len_list and base <= ptr_list[p] < base + size:
+            p = ptr_dict.bisect_left(base)
+            if p <  len_list and base <= ptr_dict.peekitem(p)[0] < base + size:
+                if state.posix.stderr.writable:
+                    addr, act = ptr_dict.peekitem(p)
+                    err_str = "\n=== Use-After-Free Plugin ===\nMemory Address:{:#x}\nInstrument Address:{:#x}\n".format(addr, act.ins_addr)
+                    state.posix.stderr.write(None ,err_str.encode())
                 return True
         return False
 
@@ -52,8 +56,9 @@ class MemoryChecker(BasePlugin):
         for act in reversed(actions):
             if act.bbl_addr != last_bbl_addr :
                 break
-            if act.type=='mem' and (act.sim_procedure is None or act.sim_procedure.display_name not in MemoryChecker.AllowList):
-                address_list.append(act.addr.ast)
+            if act.type=='mem' and \
+                (act.sim_procedure is None or act.sim_procedure.display_name not in MemoryChecker.AllowList):
+                address_list.append(act)
         return MemoryChecker.check_address_is_free(state, address_list)
 
     def step_callback(self, simgr):
