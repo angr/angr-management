26e2cb43625dd281b6e23d5ba6c481a865a1aecf
Fish <fish@cs.ucsb.edu>
2021-09-12 10:04:34 -0700

    Implement icons and auto-scroll.

--- angrmanagement/ui/widgets/qlog_widget.py
+++ angrmanagement/ui/widgets/qlog_widget.py
@@ -1,11 +1,20 @@
-from typing import List, Any
+import os
+import logging
+from typing import List, Any, Optional
 
 import PySide2
-from PySide2.QtWidgets import QTableView, QAbstractItemView
+from PySide2.QtWidgets import QTableView, QAbstractItemView, QHeaderView
 from PySide2.QtCore import QAbstractTableModel, Qt
+from PySide2.QtGui import QIcon
 
+from ...config import IMG_LOCATION
 from ...logic.threads import gui_thread_schedule
 from ...data.log import LogRecord
+
+
+class QLogIcons:
+    WARNING = QIcon(os.path.join(IMG_LOCATION, 'warning-icon.png'))
+    ERROR = QIcon(os.path.join(IMG_LOCATION, 'error-icon.png'))
 
 
 class QLogItemModel(QAbstractTableModel):
@@ -13,7 +22,7 @@ class QLogItemModel(QAbstractTableModel):
     Implements a table model for log items.
     """
 
-    Headers = ["Icon", "Timestamp", "Source", "Content"]
+    Headers = ["", "Timestamp", "Source", "Content"]
     COL_ICON = 0
     COL_TIMESTAMP = 1
     COL_SOURCE = 2
@@ -50,20 +59,29 @@ class QLogItemModel(QAbstractTableModel):
         log = self.log[row]
         col = index.column()
 
-        if role == Qt.DisplayRole:
+        if role == Qt.DisplayRole and col != QLogItemModel.COL_ICON:
             return self._get_column_text(log, col)
+        elif role == Qt.DecorationRole and col == QLogItemModel.COL_ICON:
+            return self._get_column_icon(log)
 
         return None
 
     @staticmethod
     def _get_column_text(log: LogRecord, col: int) -> Any:
         mapping = {
-            QLogItemModel.COL_ICON: lambda x: None,
             QLogItemModel.COL_TIMESTAMP: lambda x: str(x.timestamp),
             QLogItemModel.COL_SOURCE: lambda x: str(x.source),
             QLogItemModel.COL_CONTENT: lambda x: str(x.content),
         }
         return mapping[col](log)
+
+    @staticmethod
+    def _get_column_icon(log: LogRecord) -> Optional[QIcon]:
+        mapping = {
+            logging.WARNING: QLogIcons.WARNING,
+            logging.ERROR: QLogIcons.ERROR,
+        }
+        return mapping.get(log.level, None)
 
 
 class QLogWidget(QTableView):
@@ -76,16 +94,25 @@ class QLogWidget(QTableView):
 
         self.log_view = log_view
 
-        self.horizontalHeader().setVisible(True)
-        self.verticalHeader().setVisible(False)
+        hheader = self.horizontalHeader()
+        vheader = self.verticalHeader()
+        hheader.setVisible(True)
+        hheader.setStretchLastSection(True)
+        vheader.setVisible(False)
         self.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.setHorizontalScrollMode(self.ScrollPerPixel)
-        self.verticalHeader().setDefaultSectionSize(24)
+        vheader.setDefaultSectionSize(20)
         self.setShowGrid(False)
 
         self.model: QLogItemModel = QLogItemModel(self)
 
+        # True if we want the log panel to automatically scroll to bottom
+        self._auto_scroll: bool = True
+
         self.setModel(self.model)
+
+        self.setColumnWidth(0, 20)
+        hheader.setSectionResizeMode(1, QHeaderView.ResizeToContents)
 
         self.log_view.workspace.instance.log.am_subscribe(self._on_new_logrecord)
 
@@ -97,6 +124,8 @@ class QLogWidget(QTableView):
         gui_thread_schedule(self._on_new_logrecord_core, (log_record, ))
 
     def _on_new_logrecord_core(self, log_record: LogRecord=None):
+        self._before_row_insert()
+
         self.model.layoutAboutToBeChanged.emit()
         if log_record is None:
             # reload
@@ -104,3 +133,13 @@ class QLogWidget(QTableView):
         else:
             self.model.log.append(log_record)
         self.model.layoutChanged.emit()
+
+        self._after_row_insert()
+
+    def _before_row_insert(self):
+        scrollbar = self.verticalScrollBar()
+        self._auto_scroll = scrollbar.value() == scrollbar.maximum()
+
+    def _after_row_insert(self):
+        if self._auto_scroll:
+            self.scrollToBottom()
