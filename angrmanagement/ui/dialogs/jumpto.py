3d2e35d420f4d66c952a0a6a0ffd66c0a1ae9bed
Matt Borgerson <contact@mborgerson.com>
2022-01-06 14:12:15 -0700

    Use QDialogButtonBox in JumpTo dialog

--- angrmanagement/ui/dialogs/jumpto.py
+++ angrmanagement/ui/dialogs/jumpto.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QLineEdit
+from PySide2.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QDialogButtonBox, QLineEdit
 from PySide2.QtCore import Qt
 
 from ..widgets import QAddressInput
@@ -50,22 +50,13 @@ class JumpTo(QDialog):
         self._status_label = status_label
 
         # buttons
-
-        ok_button = QPushButton(self)
-        ok_button.setText('OK')
-        ok_button.setEnabled(False)
-        ok_button.clicked.connect(self._on_ok_clicked)
-        self._ok_button = ok_button
-
-        cancel_button = QPushButton(self)
-        cancel_button.setText('Cancel')
-        cancel_button.clicked.connect(self._on_cancel_clicked)
-
-        buttons_layout = QHBoxLayout()
-        buttons_layout.addWidget(ok_button)
-        buttons_layout.addWidget(cancel_button)
-
-        self.main_layout.addLayout(buttons_layout)
+        buttons = QDialogButtonBox(parent=self)
+        buttons.setStandardButtons(QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Ok)
+        buttons.accepted.connect(self._on_ok_clicked)
+        buttons.rejected.connect(self.close)
+        self._ok_button = buttons.button(QDialogButtonBox.Ok)
+        self._ok_button.setEnabled(False)
+        self.main_layout.addWidget(buttons)
 
     #
     # Event handlers
@@ -93,7 +84,3 @@ class JumpTo(QDialog):
             r = self._disasm_view.jump_to(addr)
             if r:
                 self.close()
-
-    def _on_cancel_clicked(self):
-        self.cfg_args = None
-        self.close()
