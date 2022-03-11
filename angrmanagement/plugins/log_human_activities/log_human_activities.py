35a1556193f476c5b60234ac19964fee520f5136
Fish <fish@cs.ucsb.edu>
2022-03-11 11:53:00 -0700

    Logger: info -> debug.

--- angrmanagement/plugins/log_human_activities/log_human_activities.py
+++ angrmanagement/plugins/log_human_activities/log_human_activities.py
@@ -68,7 +68,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
             created_by=self.user,
         )
         self._log_list.append(variable_rename)
-        l.info("Add variable rename sesssion to slacrs")
+        l.debug("Add variable rename sesssion to slacrs")
 
     def handle_function_rename(self, func, old_name: str, new_name: str):
         """
@@ -84,7 +84,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
             created_by=self.user,
         )
         self._log_list.append(function_rename)
-        l.info("Add function rename sesssion to slacrs, project name %s, old_name %s, new_name %s",
+        l.debug("Add function rename sesssion to slacrs, project name %s, old_name %s, new_name %s",
                 self.project_name, old_name, new_name)
 
     def handle_click_block(self, qblock, event):
@@ -96,7 +96,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
             created_by=self.user,
         )
         self._log_list.append(block_click)
-        l.info("Block %x is clicked", qblock.addr)
+        l.debug("Block %x is clicked", qblock.addr)
         return False
 
     def handle_click_insn(self, qinsn, event):
@@ -108,7 +108,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
             created_by=self.user,
         )
         self._log_list.append(insn_click)
-        l.info("Instruction %x is clicked", qinsn.addr)
+        l.debug("Instruction %x is clicked", qinsn.addr)
         return False
 
     def handle_raise_view(self, view):
@@ -130,7 +130,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
             addr=addr
         )
         self._log_list.append(raise_view)
-        l.info("View %s is raised with function %s", view_name, func_name)
+        l.debug("View %s is raised with function %s", view_name, func_name)
 
     def handle_comment_changed(self, addr: int, cmt: str, new: bool, decomp: bool):
         """
@@ -148,7 +148,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
             created_by=self.user,
         )
         self._log_list.append(comment_change)
-        l.info("Comment is added at %x", addr)
+        l.debug("Comment is added at %x", addr)
         return False
 
     def handle_project_initialization(self):
@@ -161,8 +161,8 @@ class LogHumanActivitiesPlugin(BasePlugin):
             filename = self.workspace.instance.project.filename
             self.project_name = filename
             self.project_md5 = self.workspace.instance.project.loader.main_object.md5.hex()
-            l.info("Set project md5 to %s", self.project_md5)
-        l.info("Set project name to %s", self.project_name)
+            l.debug("Set project md5 to %s", self.project_md5)
+        l.debug("Set project name to %s", self.project_name)
 
     @staticmethod
     def _get_function_from_view(view):
@@ -173,7 +173,7 @@ class LogHumanActivitiesPlugin(BasePlugin):
         return None
 
     def _commit_logs(self):
-        l.info("database: %s", Conf.checrs_backend_str)
+        l.debug("database: %s", Conf.checrs_backend_str)
         asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
         while self.active:
             try:
