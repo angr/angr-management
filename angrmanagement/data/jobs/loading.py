509f26dc8e060bd503aee602c48d7dc326336c88
mborgerson <contact@mborgerson.com>
2022-03-24 15:56:28 -0700

    Remove deprecated calls to close() on loader (#592)

--- angrmanagement/data/jobs/loading.py
+++ angrmanagement/data/jobs/loading.py
@@ -30,7 +30,6 @@ class LoadTargetJob(Job):
             partial_ld = apb.fire(return_loader=True, perform_relocations=False, load_debug_info=False)
             self._progress_callback(50)
             load_options, cfg_args, variable_recovery_args = gui_thread_schedule(LoadBinary.run, (partial_ld,))
-            partial_ld.close()
             if cfg_args is None:
                 return
 
@@ -77,7 +76,6 @@ class LoadBinaryJob(Job):
 
         self._progress_callback(50)
         new_load_options, cfg_args, variable_recovery_args = gui_thread_schedule(LoadBinary.run, (partial_ld, ))
-        partial_ld.close()
         if cfg_args is None:
             return
 
