40eca0d7c0d344e96186eef96170ee0d32edac0b
Audrey Dutcher <audrey@rhelmot.io>
2022-05-06 15:49:37 -0700

    oh my god

--- angrmanagement/ui/widgets/qinstruction.py
+++ angrmanagement/ui/widgets/qinstruction.py
@@ -126,7 +126,7 @@ class QInstruction(QCachedGraphicsItem):
         return None
 
     def load_comment(self):
-        if self.worksapce.instance.kb is None:
+        if self.workspace.instance.kb is None:
             self._comment = None
         else:
             self._comment = get_comment_for_display(self.workspace.instance.kb, self.insn.addr)
