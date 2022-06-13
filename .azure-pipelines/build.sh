0bd291c5c3e94634f27e02c56b778776473bb43d
Kevin Phoenix <kevin@kphoenix.us>
2022-06-13 16:38:31 -0700

    Pin appimage-builder to 1.0.2

--- .azure-pipelines/build.sh
+++ .azure-pipelines/build.sh
@@ -20,7 +20,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     chmod +x ~/.bin/appimagetool
     export PATH="$HOME/.bin:$PATH"
 
-    pip install "appimage-builder>=1.0.0a2"
+    pip install "appimage-builder==1.0.2"
     pip install keystone-engine
     pip install git+https://github.com/angr/archr.git#egg=archr
 fi
