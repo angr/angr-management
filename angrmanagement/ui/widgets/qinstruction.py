5679ea55e9b1285e3d1f5d29f44beff1b8848bdf
Yan <yans@yancomm.net>
2022-07-01 16:16:38 -0700

    add address offsets to QInstruction

--- angrmanagement/ui/widgets/qinstruction.py
+++ angrmanagement/ui/widgets/qinstruction.py
@@ -156,7 +156,8 @@ class QInstruction(QCachedGraphicsItem):
         self._operands.clear()
 
         # address
-        self._addr = "%08x" % self.insn.addr
+        addr_offsets = self.workspace.instance.project.loader.addr_offset_info(self.insn.addr)
+        self._addr =  str(addr_offsets) + " " + "%08x" % self.insn.addr
         self._addr_item = QGraphicsSimpleTextItem(self)
         self._addr_item.setBrush(QBrush(self._config.disasm_view_node_address_color))
         self._addr_item.setFont(self._config.disasm_font)
