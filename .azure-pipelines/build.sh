2af037127fd4ce4cfd77fec1291d1885fc5b88a2
Kevin Phoenix <kevin@kphoenix.us>
2022-10-20 15:55:33 -0700

    Try pinning setuptools version

--- .azure-pipelines/build.sh
+++ .azure-pipelines/build.sh
@@ -7,7 +7,7 @@ alias pip="python -m pip"
 
 # Install dependencies
 
-pip install -U pip wheel setuptools pyinstaller==5.5 unicorn==1.0.2rc4
+pip install -U pip wheel setuptools==65.4.1 pyinstaller==5.5 unicorn==1.0.2rc4
 pip install git+https://github.com/eliben/pyelftools#egg=pyelftools
 pip install git+https://github.com/angr/archinfo.git#egg=archinfo
 pip install git+https://github.com/angr/pyvex.git#egg=pyvex
