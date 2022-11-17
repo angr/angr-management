4f0fce89545467027982e949d6c3632c6c0e0afe
zwimer <zwimer@gmail.com>
2022-11-16 21:48:18 -0700

    Revert to master

--- angrmanagement/data/object_container.py
+++ angrmanagement/data/object_container.py
@@ -1,10 +1,4 @@
-import os
-import queue
 import logging
-import weakref
-import threading
-from typing import Optional
-from multiprocessing import Queue
 
 from ..utils.namegen import NameGenerator
 
@@ -12,50 +6,14 @@ from ..utils.namegen import NameGenerator
 l = logging.getLogger(__name__)
 
 class EventSentinel:
-    """
-    A class which exposes methods for listeners to subscribe
-    methods for callbacks when an event is triggered
-    Callbacks may be synchronous or asynchronous
-    Only asynchronous callbacks may be triggered by an event in a different process
-    """
-    def __init__(self, async_: bool = False):
-        """
-        If async_, __init__ constructs an async thread
-        :param async_: If true, callbacks triggered via am_event will asynchronous
-        """
+    def __init__(self):
         self.am_subscribers = []
-        self.am_async: bool = async_
-        # Variables only used in asynchronous mode
-        self._am_queue: Optional[Queue] = Queue() if async_ else None
-        self._am_pid: Optional[int] = os.getpid() if async_ else None
-        if self.am_async:
-            threading.Thread(
-                target=self._async_loop,
-                args=(weakref.proxy(self),),
-                daemon=True,
-            ).start()
 
-    def am_subscribe(self, listener) -> None:
-        """
-        Subscribe listener to the event
-        If async, may only be called from the process the async loop is in
-        :param listener: The listener to subscribe to the event
-        :return: None
-        """
-        if self.am_async and os.getpid() != self._am_pid:
-            raise RuntimeError("Subscribe only works from the process the async thread lives in")
+    def am_subscribe(self, listener):
         if listener is not None:
             self.am_subscribers.append(listener)
 
     def am_unsubscribe(self, listener):
-        """
-        Unsubscribe listener to the event
-        If async, may only be called from the process the async loop is in
-        :param listener: The listener to subscribe to the event
-        :return: None
-        """
-        if self.am_async and os.getpid() != self._am_pid:
-            raise RuntimeError("Unsubscribe only works from the process the async thread lives in")
         if listener is not None:
             try:
                 self.am_subscribers.remove(listener)
@@ -63,42 +21,16 @@ class EventSentinel:
                 l.warning("Double-unsubscribe of %s from %s", listener, self)
 
     def am_event(self, **kwargs):
-        if self.am_async:
-            self._am_queue.put(kwargs)  # Should not block, queue has no size limit
-        else:
-            self._am_event(kwargs)
-
-    def _am_event(self, kwargs):
         for listener in self.am_subscribers:
             try:
                 listener(**kwargs)
             except Exception:  # pylint: disable=broad-except
                 l.exception("Error raised from event of %s", self)
 
-    @staticmethod
-    def _async_loop(weak_self: weakref.ProxyType) -> None:
-        """
-        Call am_event on every item put into the queue while self exists
-        :param weak_self: A weak reference to the EventSentinel
-        :return: None
-        """
-        if weak_self.am_async is False:
-            raise RuntimeError("Must be called on an async EventSentinel")
-        try:
-            while True:
-                try:  # We loop with a timeout to recheck if weak_self is still alive
-                    weak_self._am_event(weak_self._am_queue.get(block=True, timeout=.05))
-                except queue.Empty:
-                    pass
-        except ReferenceError:
-            l.debug("Event loop for async EventSentinel died")
 
 class ObjectContainer(EventSentinel):
-    """
-    A EventSentinel wrapper for an object
-    """
-    def __init__(self, obj, name=None, notes='', async_: bool = False):
-        super().__init__(async_)
+    def __init__(self, obj, name=None, notes=''):
+        super().__init__()
         self._am_obj = None
         self.am_obj = obj
         self.am_name = name if name is not None else NameGenerator.random_name()
@@ -158,4 +90,3 @@ class ObjectContainer(EventSentinel):
 
     def __repr__(self):
         return '(container: %s)%s' % (self.am_name, repr(self._am_obj))
-
