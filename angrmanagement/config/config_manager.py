a0317ef9c87dad434c1527695165777d841e504a
Audrey Dutcher <audrey@rhelmot.io>
2022-11-17 15:11:41 -0700

    Highlight selected items with a color from the palette

--- angrmanagement/config/config_manager.py
+++ angrmanagement/config/config_manager.py
@@ -184,6 +184,7 @@ ENTRIES = [
     CE('pseudocode_function_color', QColor, QColor(0x00, 0x00, 0xff, 0xff)),
     CE('pseudocode_quotation_color', QColor, QColor(0x00, 0x80, 0x00, 0xff)),
     CE('pseudocode_keyword_color', QColor, QColor(0x00, 0x00, 0x80, 0xff)),
+    CE('pseudocode_highlight_color', QColor, QColor(0xff, 0xff, 0x00, 0xff)),
     CE('proximity_node_background_color', QColor, QColor(0xfa, 0xfa, 0xfa)),
     CE('proximity_node_selected_background_color', QColor, QColor(0xcc, 0xcc, 0xcc)),
     CE('proximity_node_border_color', QColor, QColor(0xf0, 0xf0, 0xf0)),
