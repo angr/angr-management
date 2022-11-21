4c202a2bda1624917eadec118643f47f2148a70e
Zachary Wimer <zwimer@users.noreply.github.com>
2022-11-21 11:28:43 -0700

    Allow angr-management's log widget to capture subprocess logging (#786)
    * Allow interprocess EventSentinel for logging
    
    * lint
    
    * Revert to master
    
    * Use multiprocess process initializers to install IPC-based log handler
    
    * Remove debugging code
    
    * Do not duplicate log messages on when multiprocessing mode is fork
    
    * Fix typo from previous commit
    
    * lint

--- angrmanagement/data/log.py
+++ angrmanagement/data/log.py
@@ -1,4 +1,7 @@
+from multiprocessing import Queue
 import logging
+
+from angr.utils.mp import Initializer
 
 
 class LogRecord:
@@ -28,3 +31,33 @@ class LogDumpHandler(logging.Handler):
         log_record = LogRecord(record.levelno, record.created, record.name, self.format(record))
         self.instance.log.append(log_record)
         self.instance.log.am_event(log_record=log_record)
+
+
+class _QueueHandler(logging.handlers.QueueHandler):
+    """
+    A logging QueueHandler that is of a different type than the default QueueHandler
+    This allows checking isinstance to ensure the handler is what we desired
+    """
+
+
+def install_queue_handler(queue: Queue):
+    """
+    Install a queue handler using the given queue
+    This function should work for both fork and spawn modes of multiprocessing
+    Fork modes may already have the parent logger installed, spawn may not
+    """
+    if not any(isinstance(i, _QueueHandler) for i in logging.root.handlers):
+        logging.root.handlers.insert(0, _QueueHandler(queue))
+
+
+def initialize(*args, **kwargs) -> None:
+    """
+    Installs a LogDumpHandler and sets up forwarding from other processes to this one
+    """
+    queue = Queue()
+    # Install queue handlers to the current process and all future subprocesses
+    Initializer.get().register(install_queue_handler, queue)
+    install_queue_handler(queue)
+    # Install a listener which forwards log records to the LogDumpHandler
+    primary = LogDumpHandler(*args, **kwargs)
+    logging.handlers.QueueListener(queue, primary).start()
