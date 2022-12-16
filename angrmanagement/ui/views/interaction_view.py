7a6faa99b2790ec8e37c87bb35fa7435ac510023
zwimer <zwimer@gmail.com>
2022-12-16 16:47:16 -0700

    Improve error message

--- angrmanagement/ui/views/interaction_view.py
+++ angrmanagement/ui/views/interaction_view.py
@@ -292,9 +292,12 @@ class InteractionView(BaseView):
         }
         is_missing = [ key for key, value in required.items() if value is None ]
         if len(is_missing) > 0:
-            req_msg = 'To use this feature you need to install the following:\n\n\t' + '\n\t'.join(is_missing)
-            req_msg += '\n\nInstall them to enable this functionality.'
-            req_msg += '\nRelaunch angr-management after install.'
+            if getattr(sys, "frozen", None) is None:
+                req_msg = 'To use this feature you need to install the following:\n\n\t' + '\n\t'.join(is_missing)
+                req_msg += '\n\nInstall them to enable this functionality.'
+                req_msg += '\nRelaunch angr-management after install.'
+            else:
+                req_msg = "This feature is not available on this build of angr management")
             QtWidgets.QMessageBox.critical(None, 'Dependency error', req_msg)
             return
 
