19919e0a062ee9aef536eab990d71c640517d516
Kevin Phoenix <kevin@kphoenix.us>
2023-01-13 15:52:04 -0700

    Add charset_normalizer.md__mypyc as a hidden import (#874)

--- packaging/pyinstaller/bundle.py
+++ packaging/pyinstaller/bundle.py
@@ -114,6 +114,7 @@ def make_common_options(for_chess=False):
         "--hidden-import=pyzmq",
         "--hidden-import=xmlrpc.server",
         "--hidden-import=PySide6.support.deprecated",
+        "--hidden-import=charset_normalizer.md__mypyc",
     ]
     if for_chess:
         hidden_import.append("--hidden-import=slacrs")
