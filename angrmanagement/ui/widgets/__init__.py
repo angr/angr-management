7beb16ca54e1be31183e9ad910e3e856669506a4
Matt Borgerson <contact@mborgerson.com>
2023-02-27 16:54:11 -0700

    Add QIconLabel widget

--- angrmanagement/ui/widgets/__init__.py
+++ angrmanagement/ui/widgets/__init__.py
@@ -7,6 +7,7 @@ from .qdisasm_statusbar import QDisasmStatusBar
 
 # other widgets
 from .qfeature_map import QFeatureMap
+from .qicon_label import QIconLabel
 from .qinst_annotation import QAvoidAddrAnnotation, QBlockAnnotations, QFindAddrAnnotation, QHookAnnotation
 from .qlinear_viewer import QLinearDisassembly, QLinearDisassemblyView
 from .qstate_combobox import QStateComboBox
@@ -14,15 +15,16 @@ from .qsymexec_graph import QSymExecGraph
 from .qtrace_map import QTraceMap
 
 __all__ = [
-    "QAddressInput",
     "DisassemblyLevel",
-    "QDisassemblyGraph",
-    "QDisasmStatusBar",
-    "QFeatureMap",
+    "QAddressInput",
     "QAvoidAddrAnnotation",
     "QBlockAnnotations",
+    "QDisasmStatusBar",
+    "QDisassemblyGraph",
+    "QFeatureMap",
     "QFindAddrAnnotation",
     "QHookAnnotation",
+    "QIconLabel",
     "QLinearDisassembly",
     "QLinearDisassemblyView",
     "QStateComboBox",
