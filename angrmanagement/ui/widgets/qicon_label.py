36350b9b1fb606fe97498fff971555a9159c94bf
Matt Borgerson <contact@mborgerson.com>
2023-02-27 16:54:11 -0700

    Add QIconLabel widget

---
+++ angrmanagement/ui/widgets/qicon_label.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING
+
+from PySide6.QtCore import QSize, Signal
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QWidget
+
+if TYPE_CHECKING:
+    from PySide6.QtGui import QIcon
+
+
+class QIconLabel(QWidget):
+    """
+    Show a label with an icon on the left.
+    """
+
+    clicked = Signal()
+
+    def __init__(self, icon: "QIcon", text: str = ""):
+        super().__init__()
+        lyt = QHBoxLayout()
+        lyt.setContentsMargins(0, 0, 0, 0)
+
+        self._icon_label = QLabel()
+        self._icon_label.setPixmap(icon.pixmap(QSize(16, 16)))
+        lyt.addWidget(self._icon_label)
+
+        self._text_label = QLabel(text)
+        lyt.addWidget(self._text_label)
+
+        self.setLayout(lyt)
+        self._update_visibility()
+
+    def mouseReleaseEvent(self, _):
+        self.clicked.emit()
+
+    def setText(self, text):
+        self._text_label.setText(text)
+        self._update_visibility()
+
+    def _update_visibility(self):
+        self._text_label.setVisible(self._text_label.text() != "")
