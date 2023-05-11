c3124b9c7b6c6b46fd03996b38925c3345978b8e
Kevin Phoenix <kevin@kphoenix.us>
2023-05-11 12:21:30 -0700

    Specify build and dist paths

--- packaging/pyinstaller/bundle.py
+++ packaging/pyinstaller/bundle.py
@@ -8,5 +8,9 @@ os.chdir(pathlib.Path(__file__).parent)
 common_args = [
     "--noconfirm",
     str(pathlib.Path(__file__).parent / "angr-management.spec"),
+    "--workpath",
+    "build",
+    "--distpath",
+    "dist",
 ]
 PyInstaller.__main__.run([*common_args])
