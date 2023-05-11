660343d3777329a7b7d73910900fc53e43a6691c
mahaloz <zionbasque@gmail.com>
2023-05-10 23:15:07 -0600

    code for searching

--- angrmanagement/plugins/value_search/value_search_plugin.py
+++ angrmanagement/plugins/value_search/value_search_plugin.py
@@ -1,4 +1,5 @@
 import logging
+import struct
 
 from angrmanagement.plugins import BasePlugin
 from angrmanagement.ui.views.disassembly_view import DisassemblyView
@@ -9,6 +10,10 @@ logger = logging.getLogger(__name__)
 class ValueSearch(BasePlugin):
     def __init__(self, workspace):
         super().__init__(workspace)
+
+        endness = self.workspace.main_instance.project.arch.memory_endness
+        self._endness_encoding = "<" if endness.endswith("BE") else ">"
+
         self._create_search_view()
 
     #
@@ -30,3 +35,47 @@ class ValueSearch(BasePlugin):
 
     def _destroy_search_view(self):
         self.workspace.remove_view(self.search_view)
+
+    #
+    # helpers
+    #
+
+    def search_by_bytes(self, value: bytes):
+        if value is None:
+            return []
+
+        return list(self.workspace.main_instance.project.loader.memory.find(value))
+
+    def _float_to_bytes(self, f_value: float):
+        try:
+            enc_value = struct.pack(f"{self._endness_encoding}f", f_value)
+        except:
+            enc_value = None
+
+        return enc_value
+
+    def _int_to_bytes(self, i_value: int):
+        try:
+            enc_value = struct.pack(f"{self._endness_encoding}I", i_value)
+        except:
+            enc_value = None
+
+        return enc_value
+
+    #
+    # callbacks
+    #
+
+    def on_search_trigger(self, value: str, type_: str):
+        if type_ == "int":
+            i_val = int(value, 0)
+            value = self._int_to_bytes(i_val)
+        elif type_ == "float":
+            f_val = float(value)
+            value = self._float_to_bytes(f_val)
+        else:
+            value = value.encode()
+
+        return self.search_by_bytes(value)
+
+
