1b7272cd8cb3755c307c2466e2e052174aea2010
Wil Gibbs <17516764+Cl4sm@users.noreply.github.com>
2023-05-11 08:15:55 -0700

    Add search by value functionality (#999)
    * Add search view plugin
    
    * add harness toml
    
    * code for searching
    
    * fix
    
    * Table stuff
    
    * fix
    
    * Add search table
    
    * Fix encoding
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: mahaloz <zionbasque@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ angrmanagement/plugins/value_search/search_view.py
@@ -0,0 +1,90 @@
+from typing import TYPE_CHECKING
+
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QLineEdit, QVBoxLayout, QComboBox, QPushButton
+
+from angrmanagement.ui.widgets.qstring_table import QStringTable
+from angrmanagement.plugins.value_search.qsearch_table import QSearchTable
+
+from angrmanagement.ui.views.view import BaseView
+
+if TYPE_CHECKING:
+    from angr.knowledge_plugins.cfg.memory_data import MemoryData
+
+
+class SearchView(BaseView):
+    def __init__(self, plugin, workspace, instance, default_docking_position, *args, **kwargs):
+        super().__init__("search", workspace, instance, default_docking_position, *args, **kwargs)
+
+        self.base_caption = "Search"
+        self.plugin = plugin
+
+        self._string_table: QStringTable
+        self._type_list: QComboBox
+
+        self._selected_type = "bytes"
+
+        self._init_widgets()
+        self.reload()
+
+    def reload(self):
+        pass
+
+    def sizeHint(self):
+        return QSize(400, 800)
+
+    #
+    # Event handlers
+    #
+
+    def _on_search_click(self):
+        self._selected_type = self._type_list.currentText()
+        pattern = self._filter_string.text()
+        self._value_table.filter_string = pattern
+        self.reload()
+
+    def _on_string_selected(self, s: "MemoryData"):
+        """
+        A string reference is selected.
+
+        :param s:
+        :return:
+        """
+
+        if len(self.workspace.view_manager.views_by_category["hex"]) == 1:
+            hex_view = self.workspace.view_manager.first_view_in_category("hex")
+        else:
+            hex_view = self.workspace.view_manager.current_view_in_category("hex")
+        if hex_view is not None:
+            hex_view.jump_to(s[0])
+            self.workspace.view_manager.raise_view(hex_view)
+
+    #
+    # Private methods
+    #
+
+    def _init_widgets(self):
+        self._type_list = QComboBox(parent=self)
+        self._type_list.addItems(["bytes", "int", "float"])
+        self._search_button = QPushButton("Search", parent=self)
+        self._filter_string = QLineEdit(self)
+        self._search_button.clicked.connect(self._on_search_click)
+
+        search_layout = QHBoxLayout()
+        search_layout.addWidget(QLabel("Type:", self))
+        search_layout.addWidget(self._type_list, 10)
+        search_layout.addWidget(QLabel("Query:", self))
+        search_layout.addWidget(self._filter_string, 10)
+        search_layout.addWidget(self._search_button)
+        search_layout.setContentsMargins(3, 3, 3, 3)
+        search_layout.setSpacing(3)
+
+        self._value_table = QSearchTable(self.instance, self, selection_callback=self._on_string_selected)
+
+        layout = QVBoxLayout()
+        layout.addLayout(search_layout)
+        layout.addWidget(self._value_table)
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(layout)
---
+++ angrmanagement/plugins/value_search/value_search_plugin.py
@@ -0,0 +1,87 @@
+import logging
+import struct
+
+from angrmanagement.plugins import BasePlugin
+from .search_view import SearchView
+
+logger = logging.getLogger(__name__)
+
+
+class ValueSearch(BasePlugin):
+    def __init__(self, workspace):
+        super().__init__(workspace)
+
+        self._endness_encoding = None
+        self._create_search_view()
+
+    def _find_endness_encoding(self):
+        endness = self.workspace.main_instance.project.arch.memory_endness
+        self._endness_encoding = ">" if endness.endswith("BE") else "<"
+
+    #
+    # UI Callback Handlers
+    #
+
+    def handle_click_menu(self, idx):
+        pass
+
+    def color_insn(self, addr, selected, disasm_view):
+        pass
+
+    def teardown(self):
+        self._destroy_search_view()
+
+    def _create_search_view(self):
+        self.search_view = SearchView(self, self.workspace, self.workspace.main_instance, "center")
+        self.workspace.add_view(self.search_view)
+
+    def _destroy_search_view(self):
+        self.workspace.remove_view(self.search_view)
+
+    #
+    # helpers
+    #
+
+    def search_by_bytes(self, value: bytes):
+        if value is None:
+            return []
+
+        return list(self.workspace.main_instance.project.loader.memory.find(value))
+
+    def _float_to_bytes(self, f_value: float):
+        if self._endness_encoding is None:
+            self._find_endness_encoding()
+
+        try:
+            enc_value = struct.pack(f"{self._endness_encoding}f", f_value)
+        except:
+            enc_value = None
+
+        return enc_value
+
+    def _int_to_bytes(self, i_value: int):
+        if self._endness_encoding is None:
+            self._find_endness_encoding()
+
+        try:
+            enc_value = struct.pack(f"{self._endness_encoding}I", i_value)
+        except:
+            enc_value = None
+
+        return enc_value
+
+    #
+    # callbacks
+    #
+
+    def on_search_trigger(self, value: str, type_: str):
+        if type_ == "int":
+            i_val = int(value, 0)
+            value = self._int_to_bytes(i_val)
+        elif type_ == "float":
+            f_val = float(value)
+            value = self._float_to_bytes(f_val)
+        else:
+            value = value.encode().decode("unicode_escape").encode("latin-1")
+
+        return self.search_by_bytes(value), value
