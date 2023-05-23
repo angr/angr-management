9db801db2776068548b7c57276ad7702e56ab723
Kevin Phoenix <kevin@kphoenix.us>
2023-05-23 16:20:51 -0700

    Update python for pyinstaller to 3.11 (#901)
    * Update python for pyinstaller to 3.11
    
    * Install claripy from dep/z3-4.12.2.0 branch
    
    * Bump pyinstaller to latest version
    
    * Exclude debugpy
    
    * Remove some no-longer-necessary hidden imports
    
    * Monkeypatch NullWriter when stdio is None
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Revert claripy to default branch
    
    * Fix lint
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- start.py
+++ start.py
@@ -3,7 +3,9 @@
 import multiprocessing
 
 from angrmanagement.__main__ import main
+from angrmanagement.utils.monkeypatch_stdio import monkeypatch_stdio
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
+    monkeypatch_stdio()
     main()
