ce01427d9fbe9deaede3eab8936a651df940ae13
Matt Borgerson <contact@mborgerson.com>
2023-09-18 01:46:06 -0700

    FunctionContextMenu: Add entry to show function info dialog

--- angrmanagement/ui/menus/function_context_menu.py
+++ angrmanagement/ui/menus/function_context_menu.py
@@ -1,11 +1,12 @@
 from angrmanagement.logic import GlobalInfo
 
-from .menu import Menu
+from .menu import Menu, MenuEntry
 
 
 class FunctionContextMenu(Menu):
-    def __init__(self, parent):
+    def __init__(self, workspace, parent):
         super().__init__("Function", parent=parent)
+        self.workspace = workspace
 
         self.funcs = []
 
@@ -16,9 +17,15 @@ class FunctionContextMenu(Menu):
         return self
 
     def qmenu(self, extra_entries=None):
+        self.entries = []
+        if len(self.funcs):
+            self.entries.append(
+                MenuEntry("Show Function Info", lambda: self.workspace.show_function_info(self.funcs[0]))
+            )
         if extra_entries is None:
             extra_entries = ()
         return super().qmenu(
             extra_entries=list(GlobalInfo.main_window.workspace.plugins.build_context_menu_functions(self.funcs))
-            + list(extra_entries)
+            + list(extra_entries),
+            cached=False,
         )
