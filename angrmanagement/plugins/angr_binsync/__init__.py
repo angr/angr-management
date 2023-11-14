8ccb5274a518bc0e84063ab08bff4beeb0e5dd3e
Kevin Phoenix <kevin@kphoenix.us>
2023-11-14 13:10:15 -0700

    Update

--- angrmanagement/plugins/angr_binsync/__init__.py
+++ angrmanagement/plugins/angr_binsync/__init__.py
@@ -1,9 +1,3 @@
-import logging
+from binsync.decompilers.angr.plugin import BinSyncPlugin
 
-try:
-    # we need to import the plugin to register it
-    from binsync.decompilers.angr.plugin import BinSyncPlugin  # noqa: F401
-except ImportError:
-    logging.getLogger(__name__).error(
-        "[!] BinSync is not installed, please `pip install binsync` for THIS python interpreter"
-    )
+__all__ = ["BinSyncPlugin"]
