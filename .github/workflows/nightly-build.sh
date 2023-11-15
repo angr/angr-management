57d7dae110cd9c1d6a83c18cbbaaf9f53a2fd8ac
Kevin Phoenix <kevin@kphoenix.us>
2023-11-15 15:09:45 -0700

    Build improvements for macos

--- .github/workflows/nightly-build.sh
+++ .github/workflows/nightly-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ex
 
-python -m venv .venv
+# Set AM_PYTHON if it is not set by the user
+if [ -z "$AM_PYTHON" ]; then
+    AM_PYTHON="python"
+fi
+
+"$AM_PYTHON" -m venv .venv
 if [[ "$OSTYPE" == "msys" ]]; then
     source .venv/Scripts/activate
 else
@@ -11,6 +16,12 @@ fi
 # Install dependencies
 
 python -m pip install -U pip wheel setuptools unicorn==2.0.1.post1
+if [[ "$OSTYPE" == "darwin"* && "$(uname -m)" == "arm64" ]]; then
+    EXTRA_ANGR_ARGS="--no-binary capstone --no-binary pypcode"
+else
+    EXTRA_ANGR_ARGS=""
+fi
+
 
 pip install git+https://github.com/eliben/pyelftools.git
 pip install git+https://github.com/angr/archinfo.git
@@ -18,7 +29,7 @@ pip install git+https://github.com/angr/pyvex.git
 pip install git+https://github.com/angr/cle.git#egg=cle[ar,minidump,uefi,xbe]
 pip install git+https://github.com/angr/claripy.git
 pip install git+https://github.com/angr/ailment.git
-pip install --no-build-isolation git+https://github.com/angr/angr.git#egg=angr[pcode]
+pip install $EXTRA_ANGR_ARGS --no-build-isolation git+https://github.com/angr/angr.git#egg=angr[pcode]
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     pip install git+https://github.com/angr/archr.git#egg=archr
 fi
@@ -33,9 +44,9 @@ mkdir upload
 
 # Prepare onedirs
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    mkdir /tmp/angr-management-dmg
-    cp -r dist/*.app /tmp/angr-management-dmg
-    hdiutil create upload/angr-management-macOS.dmg -volname "angr-management nightly" -srcfolder /tmp/angr-management-dmg
+    pushd dist
+    zip -r ../upload/angr-management-macOS.zip angr-management.app
+    popd
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     source /etc/os-release
     tar -C dist -czf upload/angr-management-$ID-$VERSION_ID.tar.gz angr-management
