780e22d7fe175ee9f27d69b6125d178c14612fbf
Kevin Phoenix <kevin@kphoenix.us>
2024-02-01 16:38:57 -0700

    Build capstone from source on m1 mac

--- .github/workflows/nightly-build.sh
+++ .github/workflows/nightly-build.sh
@@ -8,6 +8,13 @@ else
     source .venv/bin/activate
 fi
 
+if [[ "$OSTYPE" == "darwin"* && "$(uname -m)" == "arm64" ]]; then
+    EXTRA_ANGR_INSTALL_ARGS="--no-binary capstone"
+else
+    EXTRA_ANGR_INSTALL_ARGS=""
+fi
+
+
 # Install dependencies
 
 python -m pip install -U pip wheel setuptools unicorn==2.0.1.post1
@@ -18,7 +25,7 @@ pip install git+https://github.com/angr/pyvex.git
 pip install git+https://github.com/angr/cle.git#egg=cle[ar,minidump,uefi,xbe]
 pip install git+https://github.com/angr/claripy.git
 pip install git+https://github.com/angr/ailment.git
-pip install --no-build-isolation git+https://github.com/angr/angr.git#egg=angr[pcode]
+pip install $EXTRA_ANGR_INSTALL_ARGS --no-build-isolation git+https://github.com/angr/angr.git#egg=angr[pcode]
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     pip install git+https://github.com/angr/archr.git#egg=archr
 fi
