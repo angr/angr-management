8050627e62377e332b814d577d2ffa8f6868e1ef
Kevin Phoenix <kevin@kphoenix.us>
2024-07-23 12:06:51 -0700

    Fix job cancellation methods that depended on job.keyboard_interrupt() (#1290)

--- angrmanagement/logic/jobmanager.py
+++ angrmanagement/logic/jobmanager.py
@@ -137,6 +137,12 @@ class JobManager:
         self.jobs.append(job)
         self.jobs_queue.put(job)
 
+    def cancel_job(self, job: Job) -> None:
+        if job in self.jobs:
+            self.jobs.remove(job)
+        if self.worker_thread is not None and self.worker_thread.current_job == job:
+            self.worker_thread.keyboard_interrupt()
+
     def interrupt_current_job(self) -> None:
         """Notify the current running job that the user requested an interrupt. The job may ignore it."""
         # Due to thread scheduling, current_job reference *must* first be saved on the stack. Accessing self.current_job
