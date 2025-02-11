b7c2448ee4fd691df64c97ea959a8018b0beb593
Matt Borgerson <contact@mborgerson.com>
2025-02-11 16:35:48 -0700

    GlobalInfo: Fix some pyright complaints

--- angrmanagement/logic/__init__.py
+++ angrmanagement/logic/__init__.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from PySide6.QtCore import QThread
+
     from angrmanagement.data.library_docs import LibraryDocs
     from angrmanagement.plugins.plugin_manager import PluginManager
     from angrmanagement.ui.main_window import MainWindow
@@ -13,10 +15,10 @@ class GlobalInfo:
     Global data.
     """
 
-    gui_thread = None
-    main_window: MainWindow = None
+    gui_thread: QThread | None = None
+    main_window: MainWindow | None = None
     daemon_inst = None
     daemon_conn = None
-    headless_plugin_manager: PluginManager = None
+    headless_plugin_manager: PluginManager | None = None
     library_docs: LibraryDocs = None
     autoreload = False
