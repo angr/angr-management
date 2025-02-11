14e7e79fd0641c491551921c977c0da4cab7b687
Matt Borgerson <contact@mborgerson.com>
2025-02-11 15:43:34 -0700

    tests: Simplify, launch on main thread

--- tests/common.py
+++ tests/common.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
 import os
-import threading
-import time
 import unittest
 
 import angr
-from PySide6.QtCore import QEvent
+from PySide6.QtCore import QThread
+from PySide6.QtTest import QTest
 from PySide6.QtWidgets import QApplication
 
 from angrmanagement.config import Conf
@@ -16,8 +15,6 @@ from angrmanagement.ui.main_window import MainWindow
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "binaries", "tests")
 
 app = None
-container = {}
-thread = None
 
 
 def create_qapp():
@@ -28,76 +25,19 @@ def create_qapp():
     return app
 
 
-def _starter():
-    global container
-
-    while True:
-        # sleep until we are told to start
-        while container.get("start", False) is False:
-            time.sleep(0.5)
-        # remove the start flag
-        del container["start"]
-
-        event: threading.Event = container["event"]
-
-        main = MainWindow(show=False)
-        GlobalInfo.gui_thread = threading.get_ident()
-        GlobalInfo.is_test = True
-        container["main"] = main
-
-        while True:
-            while not GlobalInfo.events:
-                if event.is_set():
-                    break
-                time.sleep(0.1)
-            if event.is_set():
-                break
-            ev = GlobalInfo.events.pop(0)
-            if ev.type() == QEvent.User:
-                try:
-                    ev.result = ev.execute()
-                except Exception as ex:
-                    ev.exception = ex
-                ev.event.set()
-
-        main.close()
-        del main
-
-
-def start_main_window_and_event_loop(event):
-    global container, thread
-
-    container = {}
-    container["app"] = create_qapp()
-
-    if thread is None:
-        thread = threading.Thread(target=_starter, daemon=True)
-        thread.start()
-    else:
-        # reuse the existing thread
-        pass
-    container["event"] = event
-    container["start"] = True
-
-    # wait until the main window is created
-    while "main" not in container:
-        time.sleep(0.1)
-
-    return container["app"], container["main"]
-
-
 class AngrManagementTestCase(unittest.TestCase):
     """A base class for angr management test cases that starts the main window and event loop."""
 
-    event: threading.Event
     main: MainWindow
 
     def setUp(self):
-        self.event = threading.Event()
-        _, self.main = start_main_window_and_event_loop(self.event)
+        self.app = create_qapp()
+        GlobalInfo.gui_thread = QThread.currentThread()
+        self.main = MainWindow(show=False)
+        QTest.qWaitForWindowActive(self.main)
 
     def tearDown(self) -> None:
-        self.event.set()
+        self.main.close()
         del self.main
 
 
