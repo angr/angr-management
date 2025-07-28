ed355d0d2cbe027234885e08e83805e5eb135db1
Fish <ltfish@users.noreply.github.com>
2025-07-28 16:07:08 -0700

    Move block code objects into a new module. (#1471)
    * Move block code objects into a new module.
    
    * Type check and lint.

---
+++ angrmanagement/ui/widgets/block_code_objects/base_objects.py
@@ -0,0 +1,171 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont, QTextCharFormat
+
+from angrmanagement.config import Conf
+
+if TYPE_CHECKING:
+    from PySide6.QtWidgets import QGraphicsSceneMouseEvent
+
+    from angrmanagement.logic.disassembly import InfoDock
+
+
+class QBlockCodeOptions:
+    """
+    Various options to control display of QBlockCodeObj's
+    """
+
+    show_conditional_jump_targets: bool = True
+    show_variables: bool = True
+    show_variable_identifiers: bool = True
+
+
+class QBlockCodeObj:
+    """
+    Renders a generic "code" object and handles display related events.
+    Instances of this class mirror an AST structure, with references in the
+    `obj` property to whatever object should be displayed. Leaf nodes will add
+    text to the display document during render, which is handled at the top
+    level by `QBlockCode`.
+    """
+
+    obj: Any
+    infodock: InfoDock
+    parent: Any
+    options: QBlockCodeOptions
+    span: tuple[int, int] | None
+    subobjs: list[QBlockCodeObj | str]
+    _fmt_current: QTextCharFormat
+
+    def __init__(self, obj: Any, infodock: InfoDock, parent: Any, options: QBlockCodeOptions | None = None) -> None:
+        self.obj = obj
+        self.infodock = infodock
+        self.parent = parent
+        self.options = options or QBlockCodeOptions()
+        self.span = None
+        self.subobjs = []
+        self._fmt_current = None  # type:ignore
+        self.update_style()
+        self.create_subobjs(obj)
+
+    @staticmethod
+    def fmt() -> QTextCharFormat:
+        """
+        Get text char formatting for this object
+        """
+        fmt = QTextCharFormat()
+        fmt.setForeground(Conf.disasm_view_node_mnemonic_color)
+        return fmt
+
+    def update_style(self) -> None:
+        """
+        Updates current rendering style before draw
+        """
+        self._fmt_current = self.fmt()
+        if self.should_highlight():
+            self._fmt_current.setBackground(Conf.disasm_view_operand_highlight_color)
+            self._fmt_current.setFontWeight(QFont.Weight.Bold)
+
+    def should_highlight(self) -> bool:
+        """
+        Determine whether this object should be drawn with highlight
+        """
+        selected = self.infodock.selected_qblock_code_obj
+        return (selected is not None) and (selected is self or selected.obj is self.obj)
+
+    def create_subobjs(self, obj) -> None:
+        """
+        Initialize any display subobjects for this object
+        """
+
+    def recreate_subobjs(self) -> None:
+        self.subobjs.clear()
+        self.create_subobjs(self.obj)
+
+    def update(self) -> None:
+        """
+        Update self and parent objects
+        """
+        self.parent.update()
+
+    def render_to_doc(self, cursor) -> None:
+        """
+        Add each subobject to the document
+        """
+        self.update_style()
+        self.recreate_subobjs()
+        span_min = cursor.position()
+        for obj in self.subobjs:
+            if isinstance(obj, str):
+                cursor.insertText(obj, self._fmt_current)
+            else:
+                obj.render_to_doc(cursor)
+        span_max = cursor.position()
+        self.span = (span_min, span_max)
+
+    def hit_test(self, pos: int) -> bool:
+        """
+        Determine whether a character offset falls within the span of this object
+        """
+        if self.span is None:
+            return False
+        return self.span[0] <= pos < self.span[1]
+
+    def get_hit_obj(self, pos: int) -> QBlockCodeObj | None:
+        """
+        Find the leaf node for a given character offset
+        """
+        if not self.hit_test(pos):
+            return None
+        for obj in self.subobjs:
+            if not isinstance(obj, str):
+                hit = obj.get_hit_obj(pos)
+                if hit is not None:
+                    return hit
+        return self
+
+    def _add_subobj(self, obj: QBlockCodeObj | str) -> None:
+        """
+        Add display object `obj` to the list of subobjects
+        """
+        self.subobjs.append(obj)
+
+    def add_text(self, text: str) -> None:
+        """
+        Add a text leaf
+        """
+        self._add_subobj(text)
+
+    def add_variable(self, var) -> None:
+        self._add_subobj(QVariableObj(var, self.infodock, parent=self, options=self.options))
+
+    def mousePressEvent(self, event: QGraphicsSceneMouseEvent) -> None:  # pylint: disable=unused-argument
+        self.infodock.select_qblock_code_obj(self)
+        if event.button() == Qt.MouseButton.RightButton:
+            self.infodock.disasm_view.show_context_menu_for_selected_object()
+
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+        pass
+
+    @property
+    def should_highlight_line(self):
+        return any(obj.should_highlight_line for obj in self.subobjs if isinstance(obj, QBlockCodeObj))
+
+
+class QVariableObj(QBlockCodeObj):
+    """
+    Renders a variable
+    """
+
+    @staticmethod
+    def fmt() -> QTextCharFormat:
+        fmt = QTextCharFormat()
+        fmt.setForeground(Conf.disasm_view_variable_label_color)
+        return fmt
+
+    def create_subobjs(self, obj) -> None:
+        ident = "<%s>" % (obj.ident if obj.ident else "") if self.options.show_variable_identifiers else ""
+        self.add_text(obj.name + ident)
---
+++ angrmanagement/ui/widgets/block_code_objects/vex_objects.py
@@ -0,0 +1,294 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+import pyvex
+
+try:
+    import pypcode
+except ImportError:
+    pypcode = None
+
+from PySide6.QtGui import QTextCharFormat
+
+from angrmanagement.config import Conf
+
+from .base_objects import QBlockCodeObj
+
+if TYPE_CHECKING:
+    from archinfo import RegisterOffset, TmpVar
+
+
+OBJ_CLASS_TO_QBLOCKCODE_CLASS = {}
+
+
+class QIROpObj(QBlockCodeObj):
+    """
+    Renders a Lifter IR object
+    """
+
+    @staticmethod
+    def fmt() -> QTextCharFormat:
+        fmt = QTextCharFormat()
+        fmt.setForeground(Conf.disasm_view_ir_default_color)
+        return fmt
+
+    def __init__(self, obj: Any, *args, irobj=None, **kwargs) -> None:
+        self.irobj = irobj or obj
+        super().__init__(obj, *args, **kwargs)
+
+    def create_subobjs(self, obj) -> None:
+        self.add_irobj(obj.obj)
+
+    def add_irobj(self, obj) -> None:
+        subobjcls = OBJ_CLASS_TO_QBLOCKCODE_CLASS.get(type(obj), QIROpTextObj)
+        subobj = subobjcls(obj, self.infodock, parent=self, options=self.options, irobj=self.irobj)
+        self._add_subobj(subobj)
+
+
+class QIROpTextObj(QIROpObj):
+    """
+    Renders a Lifter IR object using the object's __str__, or as hexadecimal
+    if an integer type.
+    """
+
+    def create_subobjs(self, obj: Any) -> None:
+        if isinstance(obj, int):
+            self.add_text(f"{obj:#x}")
+        else:
+            self.add_text(str(obj))
+
+
+class QIrOpPcodeOp(QIROpTextObj):
+    """
+    Renders a P-code op.
+    """
+
+    def create_subobjs(self, obj: pypcode.PcodeOp) -> None:  # type:ignore[reportInvalidTypeForm]
+        self.add_text(pypcode.PcodePrettyPrinter.fmt_op(obj))  # type:ignore
+
+
+if pypcode:
+    OBJ_CLASS_TO_QBLOCKCODE_CLASS.update({pypcode.PcodeOp: QIrOpPcodeOp})
+
+
+class QIROpVexConstObj(QIROpTextObj):
+    """
+    Renders a pyvex.expr.Const
+    """
+
+    @staticmethod
+    def fmt() -> QTextCharFormat:
+        fmt = QTextCharFormat()
+        fmt.setForeground(Conf.disasm_view_operand_constant_color)
+        return fmt
+
+    def should_highlight(self) -> bool:
+        return (
+            isinstance(self.infodock.selected_qblock_code_obj, QIROpVexConstObj)
+            and self.infodock.selected_qblock_code_obj.obj._con == self.obj._con
+        )
+
+
+class VexIRTmpWrapper:
+    """
+    A wrapper class for VEX temps
+    """
+
+    __slots__ = (
+        "tid",
+        "reg_name",
+    )
+
+    tid: TmpVar
+    reg_name: str | None
+
+    def __init__(self, tid: TmpVar, reg_name: str | None = None) -> None:
+        self.tid = tid
+        self.reg_name = reg_name or (f"t{self.tid}")
+
+    def __str__(self) -> str:
+        return self.reg_name if self.reg_name is not None else "None"
+
+
+class VexIRRegWrapper:
+    """
+    A wrapper class for VEX registers
+    """
+
+    __slots__ = (
+        "offset",
+        "reg_name",
+    )
+
+    offset: RegisterOffset
+    reg_name: str | None
+
+    def __init__(self, offset: RegisterOffset, reg_name: str | None = None) -> None:
+        self.offset = offset
+        self.reg_name = reg_name or (f"offset={self.offset}")
+
+    def __str__(self) -> str:
+        return self.reg_name if self.reg_name is not None else "None"
+
+
+class QIROpVexWrTmpObj(QIROpTextObj):
+    """
+    Renders a pyvex.stmt.WrTmp
+    """
+
+    def create_subobjs(self, obj: pyvex.stmt.WrTmp) -> None:
+        irsb = self.irobj.irsb
+        self.add_irobj(VexIRTmpWrapper(obj.tmp))
+        self.add_text(" = ")
+        if isinstance(obj.data, pyvex.expr.Get):
+            reg_name = irsb.arch.translate_register_name(obj.data.offset, obj.data.result_size(irsb.tyenv) // 8)
+            self.add_irobj(VexIRRegWrapper(obj.data.offset, reg_name))  # type:ignore
+        else:
+            self.add_irobj(obj.data)
+
+
+class QIROpVexRdTmpObj(QIROpTextObj):
+    """
+    Renders a pyvex.expr.RdTmp
+    """
+
+    def create_subobjs(self, obj: pyvex.expr.RdTmp) -> None:
+        self.add_irobj(VexIRTmpWrapper(obj.tmp))
+
+
+class QIROpVexTmpObj(QIROpTextObj):
+    """
+    Renders a VEX temporary
+    """
+
+    @staticmethod
+    def fmt() -> QTextCharFormat:
+        fmt = QTextCharFormat()
+        fmt.setForeground(Conf.disasm_view_operand_color)
+        return fmt
+
+    def should_highlight(self) -> bool:
+        return (
+            isinstance(self.infodock.selected_qblock_code_obj, QIROpVexTmpObj)
+            and self.infodock.selected_qblock_code_obj.obj.tid == self.obj.tid
+        )
+
+
+class QIROpVexRegObj(QIROpTextObj):
+    """
+    Renders a VEX register
+    """
+
+    @staticmethod
+    def fmt() -> QTextCharFormat:
+        fmt = QTextCharFormat()
+        fmt.setForeground(Conf.disasm_view_operand_color)
+        return fmt
+
+    def should_highlight(self) -> bool:
+        return (
+            isinstance(self.infodock.selected_qblock_code_obj, QIROpVexRegObj)
+            and self.infodock.selected_qblock_code_obj.obj.offset == self.obj.offset
+        )
+
+
+class QIROpVexStoreObj(QIROpTextObj):
+    """
+    Renders a pyvex.stmt.Store
+    """
+
+    def create_subobjs(self, obj: pyvex.stmt.Store) -> None:
+        # "ST%s(%s) = %s" % (self.endness[-2:].lower(), self.addr, self.data)
+        self.add_text(f"ST{obj.endness[-2:].lower()}(")
+        self.add_irobj(obj.addr)
+        self.add_text(") = ")
+        self.add_irobj(obj.data)
+
+
+class QIROpVexLoadObj(QIROpTextObj):
+    """
+    Renders a pyvex.expr.Load
+    """
+
+    def create_subobjs(self, obj: pyvex.expr.Load) -> None:
+        self.add_text(f"LD{obj.end[-2:].lower()}:{obj.ty[4:]}(")
+        self.add_irobj(obj.addr)
+        self.add_text(")")
+
+
+class QIROpVexPutObj(QIROpTextObj):
+    """
+    Renders a pyvex.stmt.Put
+    """
+
+    def create_subobjs(self, obj: pyvex.stmt.Put) -> None:
+        irsb = self.irobj.irsb
+        reg_name = irsb.arch.translate_register_name(obj.offset, obj.data.result_size(irsb.tyenv) // 8)
+        self.add_text("PUT(")
+        self.add_irobj(VexIRRegWrapper(obj.offset, reg_name))  # type:ignore
+        self.add_text(") = ")
+        self.add_irobj(obj.data)
+
+
+class QIROpVexExitObj(QIROpTextObj):
+    """
+    Renders a pyvex.stmt.Exit
+    """
+
+    def create_subobjs(self, obj: pyvex.stmt.Exit) -> None:
+        irsb = self.irobj.irsb
+        arch = irsb.arch
+        reg_name = arch.translate_register_name(irsb.offsIP, arch.bits // 8)
+        self.add_text("if (")
+        self.add_irobj(obj.guard)
+        self.add_text(") { PUT(")
+        self.add_irobj(VexIRRegWrapper(obj.offsIP, reg_name))  # type:ignore
+        self.add_text(") = ")
+        self.add_irobj(obj.dst.value)
+        self.add_text("; ")
+        self.add_irobj(obj.jumpkind)
+        self.add_text(" }")
+
+
+class QIROpVexBinopObj(QIROpTextObj):
+    """
+    Renders a pyvex.expr.Binop
+    """
+
+    def create_subobjs(self, obj: pyvex.expr.Binop) -> None:
+        self.add_text(obj.op[4:])
+        self.add_text("(")
+        self.add_irobj(obj.args[0])
+        self.add_text(",")
+        self.add_irobj(obj.args[1])
+        self.add_text(")")
+
+
+class QIROpVexUnopObj(QIROpTextObj):
+    """
+    Renders a pyvex.expr.Unop
+    """
+
+    def create_subobjs(self, obj: pyvex.expr.Unop) -> None:
+        self.add_text(obj.op[4:])
+        self.add_text("(")
+        self.add_irobj(obj.args[0])
+        self.add_text(")")
+
+
+OBJ_CLASS_TO_QBLOCKCODE_CLASS.update(
+    {
+        pyvex.stmt.WrTmp: QIROpVexWrTmpObj,
+        pyvex.expr.RdTmp: QIROpVexRdTmpObj,
+        pyvex.stmt.Store: QIROpVexStoreObj,
+        pyvex.expr.Load: QIROpVexLoadObj,
+        pyvex.stmt.Put: QIROpVexPutObj,
+        pyvex.stmt.Exit: QIROpVexExitObj,
+        pyvex.expr.Const: QIROpVexConstObj,
+        pyvex.expr.Binop: QIROpVexBinopObj,
+        pyvex.expr.Unop: QIROpVexUnopObj,
+        VexIRTmpWrapper: QIROpVexTmpObj,
+        VexIRRegWrapper: QIROpVexRegObj,
+    }
+)
