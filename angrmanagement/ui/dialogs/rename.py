e6a9fd9c10a1042a143c844bd8bc1f388a263d9d
Matt Borgerson <contact@mborgerson.com>
2025-10-29 19:04:39 -0700

    RenameDialog: Drop unused argument to _on_name_changed: new_text

--- angrmanagement/ui/dialogs/rename.py
+++ angrmanagement/ui/dialogs/rename.py
@@ -79,7 +79,7 @@ class RenameDialog(QDialog):
     # Event handlers
     #
 
-    def _on_name_changed(self, new_text) -> None:  # pylint:disable=unused-argument
+    def _on_name_changed(self, _) -> None:
         if self._name_box is None:
             # initialization is not done yet
             return
