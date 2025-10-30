8aa1591f5591f1ac672c003e2ac62413e51f1130
Matt Borgerson <contact@mborgerson.com>
2025-10-29 19:04:39 -0700

    Drop unused global: PLUGIN_PATH

--- angrmanagement/consts.py
+++ angrmanagement/consts.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 
 APP_LOCATION = str(os.path.join(os.path.dirname(os.path.realpath(__file__))))
-PLUGIN_PATH = str(os.path.join(APP_LOCATION, "plugins"))
 RES_LOCATION = str(os.path.join(APP_LOCATION, "resources"))
 IMG_LOCATION = str(os.path.join(RES_LOCATION, "images"))
 FONT_LOCATION = str(os.path.join(RES_LOCATION, "fonts"))
